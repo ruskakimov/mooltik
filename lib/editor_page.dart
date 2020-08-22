206c56c5994061d2a3d077eee2cb005308b3dece
Rustem Kakimov <ruskakimov@gmail.com>
2020-08-22 17:56:55 +0800

    Switch between frames

--- lib/editor_page.dart
+++ lib/editor_page.dart
@@ -12,12 +12,40 @@ class EditorPage extends StatefulWidget {
 
 class _EditorPageState extends State<EditorPage> {
   final List<Frame> frames = [Frame(), Frame()];
+  int frameIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFDDDDDD),
-      body: Center(child: FrameCanvas(frame: frames.first)),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Center(child: FrameCanvas(frame: frames[frameIndex])),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              RaisedButton(
+                child: Text('1'),
+                onPressed: () {
+                  setState(() {
+                    frameIndex = 0;
+                  });
+                },
+              ),
+              RaisedButton(
+                child: Text('2'),
+                onPressed: () {
+                  setState(() {
+                    frameIndex = 1;
+                  });
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
