7a49bbbf6c8c7123e8e1b1987aee884f238f23eb
Rustem Kakimov <ruskakimov@gmail.com>
2020-08-22 18:51:32 +0800

    Refactor frame buttons

--- lib/editor_page.dart
+++ lib/editor_page.dart
@@ -23,29 +23,26 @@ class _EditorPageState extends State<EditorPage> {
           Expanded(
             child: Center(child: FrameCanvas(frame: frames[frameIndex])),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              RaisedButton(
-                child: Text('1'),
-                onPressed: () {
-                  setState(() {
-                    frameIndex = 0;
-                  });
-                },
-              ),
-              RaisedButton(
-                child: Text('2'),
-                onPressed: () {
-                  setState(() {
-                    frameIndex = 1;
-                  });
-                },
-              ),
-            ],
-          ),
+          _buildThumbnails(),
         ],
       ),
+    );
+  }
+
+  Row _buildThumbnails() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        for (var i = 0; i < frames.length; i++)
+          RaisedButton(
+            child: Text('${i + 1}'),
+            onPressed: () {
+              setState(() {
+                frameIndex = i;
+              });
+            },
+          )
+      ],
     );
   }
 }
