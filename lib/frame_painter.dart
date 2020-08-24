fd1ae72000ce4ea2de64ef51ee0727045c583d07
Rustem Kakimov <ruskakimov@gmail.com>
2020-08-24 21:41:53 +0800

    Sandwich frame painting between saveLayer and restore to apply erasing lines blendmode

--- lib/frame_painter.dart
+++ lib/frame_painter.dart
@@ -12,10 +12,16 @@ class FramePainter extends CustomPainter {
     // Clip paint outside canvas.
     canvas.clipRect(Rect.fromLTWH(0, 0, size.width, size.height));
 
-    // Scale image to fit given size.
+    // Save layer to erase paintings on it with `BlendMode.clear`.
+    canvas.saveLayer(Rect.fromLTWH(0, 0, size.width, size.height), Paint());
+
+    // Scale image to fit the given size.
     canvas.scale(size.width / frame.width, size.height / frame.height);
 
     frame.paint(canvas);
+
+    // Flatten layer. Combine drawing lines with erasing lines.
+    canvas.restore();
   }
 
   @override
