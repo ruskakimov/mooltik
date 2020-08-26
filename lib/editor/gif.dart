d870b9e82f73749120fb27a34311d2b2231a2078
Rustem Kakimov <ruskakimov@gmail.com>
2020-08-26 22:26:03 +0800

    Call encoder, stub frame to image converter

--- lib/editor/gif.dart
+++ lib/editor/gif.dart
@@ -1,7 +1,17 @@
+import 'package:image/image.dart';
+
 import 'frame/frame.dart';
 
 List<int> makeGif(List<Frame> frames, int fps) {
   if (frames.isEmpty) {
     throw ArgumentError.value(frames, 'frames', 'should not be empty');
   }
+
+  final encoder = GifEncoder(delay: 1000 ~/ fps);
+
+  frames.forEach((frame) => encoder.addFrame(frameToImage(frame)));
+
+  return encoder.finish();
 }
+
+Image frameToImage(Frame frame) {}
