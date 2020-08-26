2d1f1c4aec92c8518f92b6fcacdc6810483f0a0a
Rustem Kakimov <ruskakimov@gmail.com>
2020-08-26 22:15:40 +0800

    Use more popular package

--- lib/editor/gif.dart
+++ lib/editor/gif.dart
@@ -1,8 +1,6 @@
-import 'package:gifencoder/gifencoder.dart';
-
 import 'frame/frame.dart';
 
-List<int> createGif(List<Frame> frames, int fps) {
+List<int> makeGif(List<Frame> frames, int fps) {
   if (frames.isEmpty) {
     throw ArgumentError.value(frames, 'frames', 'should not be empty');
   }
