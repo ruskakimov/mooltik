fd64317f87503e3349781115ada89184073599a7
Rustem Kakimov <ruskakimov@gmail.com>
2020-09-01 17:52:54 +0800

    Force fullscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'editor/editor_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([]);
   runApp(App());
 }
 
