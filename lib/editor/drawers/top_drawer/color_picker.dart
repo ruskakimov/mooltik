3cb16669be9495460b3655573d43d59cadf351dd
Rustem Kakimov <ruskakimov@gmail.com>
2020-09-16 21:36:11 +0800

    Increase color picker tappable area

--- lib/editor/drawers/top_drawer/color_picker.dart
+++ lib/editor/drawers/top_drawer/color_picker.dart
@@ -14,6 +14,7 @@ class ColorPicker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      behavior: HitTestBehavior.opaque,
       onTap: () => _showColorPicker(context),
       child: SizedBox(
         width: 48,
