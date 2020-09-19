e1c5197509875eea3444e0e81c4ff9341a088404
Rustem Kakimov <ruskakimov@gmail.com>
2020-09-19 13:08:44 +0800

    Clean up editor page

--- lib/editor/editor_page.dart
+++ lib/editor/editor_page.dart
@@ -6,36 +6,8 @@ import 'drawers/bottom_drawer/bottom_drawer.dart';
 import 'drawers/top_drawer/top_drawer.dart';
 import 'timeline/timeline.dart';
 
-class EditorPage extends StatefulWidget {
+class EditorPage extends StatelessWidget {
   const EditorPage({Key key}) : super(key: key);
-
-  @override
-  _EditorPageState createState() => _EditorPageState();
-}
-
-class _EditorPageState extends State<EditorPage>
-    with SingleTickerProviderStateMixin {
-  @override
-  void initState() {
-    super.initState();
-
-    // _controller = AnimationController(
-    //   lowerBound: 0,
-    //   upperBound: _frames.length.toDouble(),
-    //   duration: Duration(milliseconds: 1000 ~/ _fps),
-    //   vsync: this,
-    // );
-
-    // _controller.addStatusListener((status) {
-    //   if (status == AnimationStatus.completed) {
-    //     setState(() {
-    //       _selectedFrameIndex = (_selectedFrameIndex + 1) % _frames.length;
-    //     });
-    //     _controller.reset();
-    //     _controller.forward();
-    //   }
-    // });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,21 +38,4 @@ class _EditorPageState extends State<EditorPage>
       ),
     );
   }
-
-  // Widget _buildPlayButton() {
-  //   return FloatingActionButton(
-  //     backgroundColor: Colors.red,
-  //     child: Icon(
-  //       _controller.isAnimating ? Icons.pause : Icons.play_arrow,
-  //     ),
-  //     onPressed: () {
-  //       if (_controller.isAnimating) {
-  //         _controller.stop();
-  //         setState(() {});
-  //       } else {
-  //         _controller.forward(from: _selectedFrameIndex.toDouble());
-  //       }
-  //     },
-  //   );
-  // }
 }
