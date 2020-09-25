7211c04941cf8ef07cc2ec212fc02856722a2ea6
Rustem Kakimov <ruskakimov@gmail.com>
2020-09-25 12:18:06 +0800

    Remove drawer clipper

--- lib/editor/drawers/editor_drawer.dart
+++ lib/editor/drawers/editor_drawer.dart
@@ -29,7 +29,6 @@ class _EditorDrawerState extends State<EditorDrawer>
   bool open = true;
   AnimationController _controller;
   Animation _animation;
-  CustomClipper _clipper;
 
   @override
   void initState() {
@@ -43,11 +42,6 @@ class _EditorDrawerState extends State<EditorDrawer>
       curve: Curves.easeOut,
       reverseCurve: Curves.easeIn,
     );
-    _clipper = _DrawerClipper(
-      buttonHeight: 48,
-      buttonWidth: 48,
-      rightButtonCount: widget.quickAccessButtons?.length ?? 0,
-    );
   }
 
   @override
@@ -60,16 +54,7 @@ class _EditorDrawerState extends State<EditorDrawer>
   Widget build(BuildContext context) {
     return AnimatedBuilder(
       animation: _animation,
-      child: CustomPaint(
-        painter: _ClippedShadowPainter(
-          clipper: _clipper,
-          shadow: Shadow(
-            color: Colors.black26,
-            blurRadius: 4,
-          ),
-        ),
-        child: _buildDrawerBody(),
-      ),
+      child: _buildDrawerBody(),
       builder: (context, child) {
         return Transform.translate(
           offset: Offset(0, -widget.height * _animation.value),
@@ -79,37 +64,34 @@ class _EditorDrawerState extends State<EditorDrawer>
     );
   }
 
-  ClipPath _buildDrawerBody() {
-    return ClipPath(
-      clipper: _clipper,
-      child: Container(
-        width: double.infinity,
-        color: Colors.blueGrey[800],
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            RepaintBoundary(
-              child: SizedBox(
-                height: widget.height,
-                child: widget.body,
+  Widget _buildDrawerBody() {
+    return Container(
+      width: double.infinity,
+      color: Colors.blueGrey[800],
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          RepaintBoundary(
+            child: SizedBox(
+              height: widget.height,
+              child: widget.body,
+            ),
+          ),
+          Row(
+            children: [
+              DrawerIconButton(
+                icon: open
+                    ? FontAwesomeIcons.chevronUp
+                    : FontAwesomeIcons.chevronDown,
+                onTap: _toggleDrawer,
               ),
-            ),
-            Row(
-              children: [
-                DrawerIconButton(
-                  icon: open
-                      ? FontAwesomeIcons.chevronUp
-                      : FontAwesomeIcons.chevronDown,
-                  onTap: _toggleDrawer,
-                ),
-                Spacer(),
-                if (widget.quickAccessButtons != null)
-                  ...widget.quickAccessButtons,
-              ],
-            ),
-          ],
-        ),
+              Spacer(),
+              if (widget.quickAccessButtons != null)
+                ...widget.quickAccessButtons,
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -124,64 +106,4 @@ class _EditorDrawerState extends State<EditorDrawer>
       _controller.forward();
     }
   }
-}
-
-class _DrawerClipper extends CustomClipper<Path> {
-  _DrawerClipper({
-    @required this.buttonHeight,
-    @required this.buttonWidth,
-    this.rightButtonCount = 0,
-  }) : super();
-
-  final double buttonHeight;
-  final double buttonWidth;
-  final int rightButtonCount;
-
-  @override
-  Path getClip(Size size) {
-    return Path()
-      ..moveTo(0, 0)
-      ..lineTo(size.width, 0)
-      ..lineTo(size.width, size.height)
-      // Start of outwards curve.
-      ..lineTo(size.width - buttonWidth * rightButtonCount + outerBorderRadius,
-          size.height)
-      ..relativeQuadraticBezierTo(
-          -outerBorderRadius, 0, -outerBorderRadius, -outerBorderRadius)
-      // Start of inwards curve.
-      ..lineTo(size.width - buttonWidth * rightButtonCount,
-          size.height - buttonHeight + innerBorderRadius)
-      ..relativeQuadraticBezierTo(
-          0, -innerBorderRadius, -innerBorderRadius, -innerBorderRadius)
-      // Start of inwards curve.
-      ..lineTo(buttonWidth + innerBorderRadius, size.height - buttonHeight)
-      ..relativeQuadraticBezierTo(
-          -innerBorderRadius, 0, -innerBorderRadius, innerBorderRadius)
-      // Start of outwards curve.
-      ..lineTo(buttonWidth, size.height - outerBorderRadius)
-      ..relativeQuadraticBezierTo(
-          0, outerBorderRadius, -outerBorderRadius, outerBorderRadius)
-      ..lineTo(0, size.height)
-      ..close();
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
-}
-
-class _ClippedShadowPainter extends CustomPainter {
-  final Shadow shadow;
-  final CustomClipper<Path> clipper;
-
-  _ClippedShadowPainter({@required this.shadow, @required this.clipper});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint = shadow.toPaint();
-    var clipPath = clipper.getClip(size).shift(shadow.offset);
-    canvas.drawPath(clipPath, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
