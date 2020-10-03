7b839a6d79e6240cc35f2ef277b0b0b4460a73dc
Rustem Kakimov <ruskakimov@gmail.com>
2020-10-04 00:52:39 +0800

    Test single animation playthough

--- test/timeline_model_test.dart
+++ test/timeline_model_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mooltik/editor/frame/frame_model.dart';
 import 'package:mooltik/editor/timeline/timeline_model.dart';
+import 'package:quiver/testing/async.dart';
 
 void main() {
   group('TimelineModel', () {
@@ -13,6 +14,28 @@ void main() {
       expect(model.playing, isTrue);
       model.stop();
       expect(model.playing, isFalse);
+    });
+
+    test('should show frames in sequence with correct duration', () {
+      final model = TimelineModel(initialKeyframes: [
+        FrameModel()..duration = 24,
+        FrameModel()..duration = 12,
+      ]);
+
+      FakeAsync().run((async) {
+        model.play();
+        expect(model.selectedKeyframeId, 0);
+        async.elapse(Duration(milliseconds: 999));
+        expect(model.selectedKeyframeId, 0);
+        async.elapse(Duration(milliseconds: 1));
+
+        expect(model.selectedKeyframeId, 1);
+        async.elapse(Duration(milliseconds: 499));
+        expect(model.selectedKeyframeId, 1);
+        async.elapse(Duration(milliseconds: 1));
+        expect(model.selectedKeyframeId, 0);
+        model.stop();
+      });
     });
   });
 }
