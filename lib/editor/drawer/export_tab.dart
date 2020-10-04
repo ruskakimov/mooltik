07f1ce960073139fc8acae193688f440aad43a3b
Rustem Kakimov <ruskakimov@gmail.com>
2020-10-04 17:27:47 +0800

    Show Saving... text while saving

--- lib/editor/drawer/export_tab.dart
+++ lib/editor/drawer/export_tab.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:mooltik/editor/gif.dart';
@@ -8,11 +9,23 @@ import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
-class ExportTab extends StatelessWidget {
+class ExportTab extends StatefulWidget {
   const ExportTab({Key key}) : super(key: key);
 
   @override
+  _ExportTabState createState() => _ExportTabState();
+}
+
+class _ExportTabState extends State<ExportTab> {
+  bool _saving = false;
+
+  @override
   Widget build(BuildContext context) {
+    if (_saving)
+      return Center(
+        child: Text('Saving...'),
+      );
+
     final timeline = context.watch<TimelineModel>();
     return Center(
       child: RaisedButton(
@@ -24,6 +37,10 @@ class ExportTab extends StatelessWidget {
           ),
         ),
         onPressed: () async {
+          setState(() {
+            _saving = true;
+          });
+
           if (await Permission.storage.request().isGranted) {
             final bytes = await makeGif(timeline.keyframes);
             final dir = await getTemporaryDirectory();
@@ -31,6 +48,10 @@ class ExportTab extends StatelessWidget {
             await file.writeAsBytes(bytes);
             await ImageGallerySaver.saveFile(file.path);
           }
+
+          setState(() {
+            _saving = false;
+          });
         },
       ),
     );
