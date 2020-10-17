a9b7881fba306b57b329ec6ca0be1f6506bc7415
Rustem Kakimov <ruskakimov@gmail.com>
2020-10-18 00:07:56 +0800

    Fix gif test

--- test/gif_test.dart
+++ test/gif_test.dart
@@ -3,6 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Throws exception if no frames are given', () {
-    expect(() => makeGif([]), throwsArgumentError);
+    expect(() => makeGif([], []), throwsArgumentError);
   });
 }
