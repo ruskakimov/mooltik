6404d14d04a4faf480979f4104bc8c3cea88ba5c
Rustem Kakimov <ruskakimov@gmail.com>
2020-10-18 00:32:11 +0800

    Fix tests

--- test/reel_model_test.dart
+++ test/reel_model_test.dart
@@ -1,14 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mooltik/editor/frame/frame_model.dart';
 import 'package:mooltik/editor/reel/reel_model.dart';
 import 'package:quiver/testing/async.dart';
 
 void main() {
-  group('reelModel', () {
+  FrameModel a, b, c, d, e, f;
+
+  setUp(() {
+    a = FrameModel(size: Size(1280, 720));
+    b = FrameModel(size: Size(1280, 720));
+    c = FrameModel(size: Size(1280, 720));
+    d = FrameModel(size: Size(1280, 720));
+    e = FrameModel(size: Size(1280, 720));
+    f = FrameModel(size: Size(1280, 720));
+  });
+
+  group('ReelModel', () {
     test('should toggle playing state on play/stop', () {
       final model = ReelModel(initialFrames: [
-        FrameModel()..duration = 24,
-        FrameModel()..duration = 12,
+        a,
+        null,
+        b,
+        null,
       ]);
       model.play();
       expect(model.playing, isTrue);
@@ -18,95 +32,105 @@ void main() {
 
     test('should show frames in sequence with correct duration (2 frames)', () {
       final model = ReelModel(initialFrames: [
-        FrameModel()..duration = 24,
-        FrameModel()..duration = 12,
+        a,
+        ...List(23),
+        b,
+        ...List(11),
       ]);
 
       FakeAsync().run((async) {
         model.play();
-        expect(model.selectedFrameId, 0);
+        expect(model.visibleFrame, a);
         async.elapse(Duration(milliseconds: 999));
-        expect(model.selectedFrameId, 0);
+        expect(model.visibleFrame, a);
         async.elapse(Duration(milliseconds: 1));
 
-        expect(model.selectedFrameId, 1);
+        expect(model.visibleFrame, b);
         async.elapse(Duration(milliseconds: 499));
-        expect(model.selectedFrameId, 1);
+        expect(model.visibleFrame, b);
         async.elapse(Duration(milliseconds: 1));
 
-        expect(model.selectedFrameId, 0);
+        expect(model.visibleFrame, a);
         model.stop();
       });
     });
 
     test('should show frames in sequence with correct duration (5 frames)', () {
       final model = ReelModel(initialFrames: [
-        FrameModel()..duration = 6,
-        FrameModel()..duration = 12,
-        FrameModel()..duration = 6,
-        FrameModel()..duration = 48,
-        FrameModel()..duration = 6,
+        a,
+        ...List(5),
+        b,
+        ...List(11),
+        c,
+        ...List(5),
+        d,
+        ...List(47),
+        e,
+        ...List(5),
       ]);
 
       FakeAsync().run((async) {
         model.play();
-        expect(model.selectedFrameId, 0);
+        expect(model.visibleFrame, a);
         async.elapse(Duration(milliseconds: 249));
-        expect(model.selectedFrameId, 0);
+        expect(model.visibleFrame, a);
         async.elapse(Duration(milliseconds: 1));
 
-        expect(model.selectedFrameId, 1);
+        expect(model.visibleFrame, b);
         async.elapse(Duration(milliseconds: 499));
-        expect(model.selectedFrameId, 1);
+        expect(model.visibleFrame, b);
         async.elapse(Duration(milliseconds: 1));
 
-        expect(model.selectedFrameId, 2);
+        expect(model.visibleFrame, c);
         async.elapse(Duration(milliseconds: 249));
-        expect(model.selectedFrameId, 2);
+        expect(model.visibleFrame, c);
         async.elapse(Duration(milliseconds: 1));
 
-        expect(model.selectedFrameId, 3);
+        expect(model.visibleFrame, d);
         async.elapse(Duration(milliseconds: 1999));
-        expect(model.selectedFrameId, 3);
+        expect(model.visibleFrame, d);
         async.elapse(Duration(milliseconds: 1));
 
-        expect(model.selectedFrameId, 4);
+        expect(model.visibleFrame, e);
         async.elapse(Duration(milliseconds: 249));
-        expect(model.selectedFrameId, 4);
+        expect(model.visibleFrame, e);
         async.elapse(Duration(milliseconds: 1));
 
-        expect(model.selectedFrameId, 0);
+        expect(model.visibleFrame, a);
         model.stop();
       });
     });
 
     test('should loop animation correctly', () {
       final model = ReelModel(initialFrames: [
-        FrameModel()..duration = 24,
-        FrameModel()..duration = 12,
-        FrameModel()..duration = 6,
+        a,
+        ...List(23),
+        b,
+        ...List(11),
+        c,
+        ...List(5),
       ]);
 
       FakeAsync().run((async) {
         model.play();
 
         void loop() {
-          expect(model.selectedFrameId, 0);
+          expect(model.visibleFrame, a);
           async.elapse(Duration(milliseconds: 999));
-          expect(model.selectedFrameId, 0);
+          expect(model.visibleFrame, a);
           async.elapse(Duration(milliseconds: 1));
 
-          expect(model.selectedFrameId, 1);
+          expect(model.visibleFrame, b);
           async.elapse(Duration(milliseconds: 499));
-          expect(model.selectedFrameId, 1);
+          expect(model.visibleFrame, b);
           async.elapse(Duration(milliseconds: 1));
 
-          expect(model.selectedFrameId, 2);
+          expect(model.visibleFrame, c);
           async.elapse(Duration(milliseconds: 249));
-          expect(model.selectedFrameId, 2);
+          expect(model.visibleFrame, c);
           async.elapse(Duration(milliseconds: 1));
 
-          expect(model.selectedFrameId, 0);
+          expect(model.visibleFrame, a);
         }
 
         loop();
@@ -122,12 +146,18 @@ void main() {
 
     test('should notify listeners at the right time, with the right id', () {
       final model = ReelModel(initialFrames: [
-        FrameModel()..duration = 2,
-        FrameModel()..duration = 2,
-        FrameModel()..duration = 2,
-        FrameModel()..duration = 2,
-        FrameModel()..duration = 2,
-        FrameModel()..duration = 2,
+        a,
+        null,
+        b,
+        null,
+        c,
+        null,
+        d,
+        null,
+        e,
+        null,
+        f,
+        null,
       ]);
 
       FakeAsync().run((async) {
@@ -146,7 +176,7 @@ void main() {
 
         async.elapse(Duration(seconds: 2));
         expect(timestamps,
-            '0-0;1-83;2-166;3-249;4-332;5-415;0-498;1-581;2-664;3-747;4-830;5-913;0-996;1-1079;2-1162;3-1245;4-1328;5-1411;0-1494;1-1577;2-1660;3-1743;4-1826;5-1909;0-1992;');
+            '0-0;1-41;2-83;3-124;4-166;5-208;6-249;7-291;8-333;9-374;10-416;11-458;0-499;1-541;2-583;3-624;4-666;5-708;6-749;7-791;8-833;9-874;10-916;11-958;0-999;1-1041;2-1083;3-1124;4-1166;5-1208;6-1249;7-1291;8-1333;9-1374;10-1416;11-1458;0-1499;1-1541;2-1583;3-1624;4-1666;5-1708;6-1749;7-1791;8-1833;9-1874;10-1916;11-1958;0-1999;');
 
         model.stop();
       });
