8d894041626c060f5c0de8e2abfa2a844a7eda24
Rustem Kakimov <ruskakimov@gmail.com>
2020-10-19 22:04:06 +0800

    Show duration in seconds

--- lib/editor/reel/frame_thumbnail.dart
+++ lib/editor/reel/frame_thumbnail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mooltik/editor/frame/frame_model.dart';
 import 'package:mooltik/editor/frame/frame_painter.dart';
 
@@ -39,19 +40,71 @@ class FrameThumbnail extends StatelessWidget {
                   ? Colors.amber.withOpacity(0.8)
                   : Colors.transparent,
             ),
-            child: duration != null
-                ? Center(
-                    child: Text(
-                      '$duration',
-                      style: TextStyle(
-                        fontSize: 42,
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                  )
-                : null,
+            child:
+                duration != null ? DurationIndicator(duration: duration) : null,
           ),
       ],
     );
+  }
+}
+
+class DurationIndicator extends StatelessWidget {
+  const DurationIndicator({
+    Key key,
+    @required this.duration,
+  }) : super(key: key);
+
+  final int duration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          children: [
+            Icon(
+              FontAwesomeIcons.angleUp,
+              color: Colors.blueGrey[900],
+              size: 12,
+            ),
+            Spacer(),
+            Icon(
+              FontAwesomeIcons.angleDown,
+              color: Colors.blueGrey[900],
+              size: 12,
+            ),
+          ],
+        ),
+        Spacer(),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              '$duration',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w900,
+                color: Colors.blueGrey[900],
+              ),
+            ),
+            SizedBox(height: 4),
+            Text(
+              time(duration),
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w900,
+                color: Colors.blueGrey[900],
+              ),
+            ),
+          ],
+        ),
+        SizedBox(width: 4),
+      ],
+    );
+  }
+
+  String time(int frames) {
+    return '${(frames / 24).toStringAsFixed(2)}s';
   }
 }
