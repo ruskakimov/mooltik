eff1351ecc9dc4660eaf7a32b85109de871a1ddd
Rustem Kakimov <ruskakimov@gmail.com>
2020-10-25 16:53:27 +0800

    Custom color picker layout

--- lib/editor/drawer/color_picker_drawer.dart
+++ lib/editor/drawer/color_picker_drawer.dart
@@ -17,19 +17,105 @@ class ColorPickerDrawer extends StatelessWidget {
     final toolbox = context.watch<ToolboxModel>();
 
     return AnimatedRightDrawer(
-      width: 600,
+      width: 400,
       open: open,
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(16.0),
         child: ColorPicker(
           pickerColor: toolbox.selectedColor,
           onColorChanged: (color) {
             toolbox.selectColor(color);
           },
           showLabel: false,
-          pickerAreaHeightPercent: 0.5,
         ),
       ),
+    );
+  }
+}
+
+class ColorPicker extends StatefulWidget {
+  const ColorPicker({
+    @required this.pickerColor,
+    @required this.onColorChanged,
+    this.paletteType: PaletteType.hsv,
+    this.enableAlpha: true,
+    this.showLabel: true,
+    this.labelTextStyle,
+    this.displayThumbColor: false,
+    this.pickerAreaBorderRadius: const BorderRadius.all(Radius.circular(4)),
+  });
+
+  final Color pickerColor;
+  final ValueChanged<Color> onColorChanged;
+  final PaletteType paletteType;
+  final bool enableAlpha;
+  final bool showLabel;
+  final TextStyle labelTextStyle;
+  final bool displayThumbColor;
+  final BorderRadius pickerAreaBorderRadius;
+
+  @override
+  _ColorPickerState createState() => _ColorPickerState();
+}
+
+class _ColorPickerState extends State<ColorPicker> {
+  HSVColor currentHsvColor = const HSVColor.fromAHSV(0.0, 0.0, 0.0, 0.0);
+
+  @override
+  void initState() {
+    super.initState();
+    currentHsvColor = HSVColor.fromColor(widget.pickerColor);
+  }
+
+  @override
+  void didUpdateWidget(ColorPicker oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    currentHsvColor = HSVColor.fromColor(widget.pickerColor);
+  }
+
+  Widget colorPickerSlider(TrackType trackType) {
+    return ColorPickerSlider(
+      trackType,
+      currentHsvColor,
+      (HSVColor color) {
+        setState(() => currentHsvColor = color);
+        widget.onColorChanged(currentHsvColor.toColor());
+      },
+      displayThumbColor: widget.displayThumbColor,
+    );
+  }
+
+  Widget colorPickerArea() {
+    return ClipRRect(
+      borderRadius: widget.pickerAreaBorderRadius,
+      child: ColorPickerArea(
+        currentHsvColor,
+        (HSVColor color) {
+          setState(() => currentHsvColor = color);
+          widget.onColorChanged(currentHsvColor.toColor());
+        },
+        widget.paletteType,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          child: colorPickerArea(),
+        ),
+        SizedBox(
+          height: 48,
+          child: colorPickerSlider(TrackType.hue),
+        ),
+        SizedBox(
+          height: 48,
+          child: colorPickerSlider(TrackType.alpha),
+        ),
+      ],
     );
   }
 }
