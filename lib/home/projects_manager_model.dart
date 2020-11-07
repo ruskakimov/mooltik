095e7d9fab62ef202ee37bfc84891544fa6f49e1
Rustem Kakimov <ruskakimov@gmail.com>
2020-11-07 18:26:05 +0800

    Extract id generator

--- lib/home/projects_manager_model.dart
+++ lib/home/projects_manager_model.dart
@@ -6,21 +6,21 @@ import 'package:path/path.dart' as p;
 class ProjectsManagerModel extends ChangeNotifier {
   Directory _directory;
   List<Project> _projects;
-  int _nextId;
+  IdGenerator _idGenerator;
 
   int get numberOfProjects => _projects?.length;
 
   Future<void> init(Directory directory) async {
     _directory = directory;
     _projects = await _readProjects();
-    _nextId = await _readNextId();
+    _idGenerator = IdGenerator(directory);
     notifyListeners();
   }
 
   Future<List<Project>> _readProjects() async {
     final List<Project> projects = [];
     await for (final FileSystemEntity dir in _directory.list()) {
-      if (dir is Directory && p.basename(dir.path).startsWith('project_')) {
+      if (dir is Directory && _validProjectDirectory(dir)) {
         projects.add(Project(dir));
       }
     }
@@ -29,16 +29,9 @@ class ProjectsManagerModel extends ChangeNotifier {
     return projects;
   }
 
-  Future<int> _readNextId() async {
-    final File idFile = File(p.join(_directory.path, 'next_id.txt'));
-
-    if (!await idFile.exists()) {
-      await idFile.create();
-      await idFile.writeAsString('0');
-    }
-
-    final String contents = await idFile.readAsString();
-    return int.parse(contents);
+  bool _validProjectDirectory(Directory directory) {
+    final String folderName = p.basename(directory.path);
+    return RegExp(r'^project_[0-9]+$').hasMatch(folderName);
   }
 
   Project getProject(int index) => _projects[index];
@@ -46,7 +39,8 @@ class ProjectsManagerModel extends ChangeNotifier {
   Future<Project> addProject() async {
     if (_projects == null) throw Exception('Read projects first.');
 
-    final String folderName = 'project_${_projects.length}';
+    final int id = await _idGenerator.generateId();
+    final String folderName = 'project_$id';
     final Directory dir =
         await Directory(p.join(_directory.path, folderName)).create();
     final Project project = Project(dir);
@@ -61,5 +55,33 @@ class ProjectsManagerModel extends ChangeNotifier {
     final Project project = _projects.removeAt(index);
     await project.directory.delete(recursive: true);
     notifyListeners();
+  }
+}
+
+class IdGenerator {
+  IdGenerator(this.directory)
+      : _file = File(p.join(directory.path, 'last_id.txt'));
+
+  final Directory directory;
+  final File _file;
+
+  Future<int> generateId() async {
+    final int id = await _readLastId() + 1;
+    _writeLastId(id);
+    return id;
+  }
+
+  Future<int> _readLastId() async {
+    if (!await _file.exists()) return -1;
+
+    final String contents = await _file.readAsString();
+    return int.parse(contents);
+  }
+
+  Future<void> _writeLastId(int lastId) async {
+    if (!await _file.exists()) {
+      await _file.create();
+    }
+    await _file.writeAsString('$lastId');
   }
 }
