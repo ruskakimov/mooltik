cd7021936a1c914125c6914cb70a8765e396c119
Rustem Kakimov <ruskakimov@gmail.com>
2020-11-07 17:25:50 +0800

    Store next id file

--- lib/home/projects_manager_model.dart
+++ lib/home/projects_manager_model.dart
@@ -6,12 +6,15 @@ import 'package:path/path.dart' as p;
 class ProjectsManagerModel extends ChangeNotifier {
   Directory _directory;
   List<Project> _projects;
+  int _nextId;
 
   int get numberOfProjects => _projects?.length;
 
   Future<void> init(Directory directory) async {
     _directory = directory;
     await _readProjects();
+    await _readNextId();
+    notifyListeners();
   }
 
   Future<void> _readProjects() async {
@@ -21,11 +24,20 @@ class ProjectsManagerModel extends ChangeNotifier {
         _projects.add(Project(dir));
       }
     }
-
     // Recent projects first.
     _projects.sort((p1, p2) => p2.id - p1.id);
+  }
 
-    notifyListeners();
+  Future<void> _readNextId() async {
+    final File idFile = File(p.join(_directory.path, 'next_id.txt'));
+
+    if (!await idFile.exists()) {
+      await idFile.create();
+      await idFile.writeAsString('0');
+    }
+
+    final String contents = await idFile.readAsString();
+    _nextId = int.parse(contents);
   }
 
   Project getProject(int index) => _projects[index];
