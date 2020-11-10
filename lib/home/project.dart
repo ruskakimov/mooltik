1b4726e3ab8088f0b99b79812bfad81a8c50e803
Rustem Kakimov <ruskakimov@gmail.com>
2020-11-10 16:40:01 +0800

    Read proejct data

--- lib/home/project.dart
+++ lib/home/project.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:mooltik/editor/reel/reel_model.dart';
 import 'package:path/path.dart' as p;
 
+// Scenario 1: new project
+// Scenario 2: existing project (read from files)
 class Project {
   Project(this.directory)
       : id = int.parse(p.basename(directory.path).split('_').last);
@@ -14,12 +17,23 @@ class Project {
   ReelModel _reel;
 
   Future<void> open() async {
-    // TODO: Read project_data.json
+    final File dataFile = File(p.join(directory.path, 'project_data.json'));
+
+    if (await dataFile.exists()) {
+      final String contents = await dataFile.readAsString();
+      _projectData = _ProjectData.fromJson(jsonDecode(contents));
+    }
   }
 }
 
 class _ProjectData {
   _ProjectData(this.width, this.height, this.drawings, this.layers);
+
+  _ProjectData.fromJson(Map<String, dynamic> json)
+      : width = json['width'],
+        height = json['height'],
+        drawings = json['drawings'],
+        layers = json['layers'];
 
   final double width;
   final double height;
