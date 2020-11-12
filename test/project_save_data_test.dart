cd9707441c7d6b2af35a0a34d209c864a6aa5027
Rustem Kakimov <ruskakimov@gmail.com>
2020-11-12 13:54:55 +0800

    Rename test

--- test/project_save_data_test.dart
+++ test/project_save_data_test.dart
@@ -4,15 +4,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mooltik/home/project_save_data.dart';
 
 void main() {
-  test('ProjectData should encode', () {
-    final data = ProjectSaveData(
-      width: 200,
-      height: 100,
-      drawings: [DrawingSaveData(id: 0, duration: 3)],
-    );
-    expect(
-      jsonEncode(data),
-      '{"width":200.0,"height":100.0,"drawings":[{"id":0,"duration":3}],"layers":[{"id":0}]}',
-    );
+  group('ProjectSaveData should', () {
+    test('encode one drawing', () {
+      final data = ProjectSaveData(
+        width: 200,
+        height: 100,
+        drawings: [DrawingSaveData(id: 0, duration: 3)],
+      );
+      expect(
+        jsonEncode(data),
+        '{"width":200.0,"height":100.0,"drawings":[{"id":0,"duration":3}],"layers":[{"id":0}]}',
+      );
+    });
   });
 }
