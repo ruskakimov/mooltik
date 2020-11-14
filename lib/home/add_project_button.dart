f1c76f29f181d8a893b7bf9844ae7ea9c9c1c04f
Rustem Kakimov <ruskakimov@gmail.com>
2020-11-14 22:11:21 +0800

    Open project on creation

--- lib/home/add_project_button.dart
+++ lib/home/add_project_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mooltik/editor/editor_page.dart';
+import 'package:mooltik/home/project.dart';
 import 'package:provider/provider.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mooltik/home/projects_manager_model.dart';
@@ -19,7 +21,18 @@ class AddProjectButton extends StatelessWidget {
         size: 18,
         color: Theme.of(context).colorScheme.onPrimary,
       ),
-      onPressed: manager.addProject,
+      onPressed: () async {
+        final project = await manager.addProject();
+        await project.open();
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => ChangeNotifierProvider<Project>.value(
+              value: project,
+              child: EditorPage(reel: project.reel),
+            ),
+          ),
+        );
+      },
     );
   }
 }
