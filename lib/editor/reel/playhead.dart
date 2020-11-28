aeca72cc0b8607e2323f8b3aed246082016cd1d6
Rustem Kakimov <ruskakimov@gmail.com>
2020-11-28 19:39:45 +0800

    Extract playhead into a file

---
+++ lib/editor/reel/playhead.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:mooltik/editor/reel/reel_model.dart';
+import 'package:provider/provider.dart';
+
+class Playhead extends StatelessWidget {
+  const Playhead({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Duration playheadPosition = context.select<ReelModel, Duration>(
+      (ReelModel reel) => reel.playheadPosition,
+    );
+
+    return Center(
+      child: Row(
+        children: [
+          Material(
+            color: Theme.of(context).colorScheme.primary,
+            borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(4),
+              bottomRight: Radius.circular(4),
+            ),
+            elevation: 1,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 6),
+              child: Text(
+                durationToLabel(playheadPosition),
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: SizedBox(
+              height: 2,
+              child: Material(
+                color: Theme.of(context).colorScheme.primary,
+                elevation: 1,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String durationToLabel(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String min = twoDigits(duration.inMinutes);
+    String sec = twoDigits(duration.inSeconds.remainder(60));
+    String secFr = twoDigits(duration.inMilliseconds.remainder(1000) ~/ 10);
+    return '$min:$sec.$secFr';
+  }
+}
--- lib/editor/reel/reel_drawer.dart
+++ lib/editor/reel/reel_drawer.dart
@@ -4,6 +4,7 @@ import 'package:flutter_portal/flutter_portal.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mooltik/editor/drawer/animated_drawer.dart';
 import 'package:mooltik/editor/frame/frame_model.dart';
+import 'package:mooltik/editor/reel/playhead.dart';
 import 'package:mooltik/editor/reel/reel_context_menu.dart';
 import 'package:provider/provider.dart';
 import 'package:mooltik/editor/reel/reel_model.dart';
@@ -117,62 +118,6 @@ class _ReelDrawerState extends State<ReelDrawer> {
         },
       );
     });
-  }
-}
-
-class Playhead extends StatelessWidget {
-  const Playhead({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final Duration playheadPosition = context.select<ReelModel, Duration>(
-      (ReelModel reel) => reel.playheadPosition,
-    );
-
-    return Center(
-      child: Row(
-        children: [
-          Material(
-            color: Theme.of(context).colorScheme.primary,
-            borderRadius: const BorderRadius.only(
-              topRight: Radius.circular(4),
-              bottomRight: Radius.circular(4),
-            ),
-            elevation: 1,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 6),
-              child: Text(
-                durationToLabel(playheadPosition),
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onPrimary,
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: SizedBox(
-              height: 2,
-              child: Material(
-                color: Theme.of(context).colorScheme.primary,
-                elevation: 1,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  String durationToLabel(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, "0");
-    String min = twoDigits(duration.inMinutes);
-    String sec = twoDigits(duration.inSeconds.remainder(60));
-    String secFr = twoDigits(duration.inMilliseconds.remainder(1000) ~/ 10);
-    return '$min:$sec.$secFr';
   }
 }
 
