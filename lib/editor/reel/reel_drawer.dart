59c30d73b430aed8df6fd9cfb2c9c3d59f13ab9b
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-03 10:36:09 +0800

    Remove unused import

--- lib/editor/reel/reel_drawer.dart
+++ lib/editor/reel/reel_drawer.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_portal/flutter_portal.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mooltik/editor/drawer/animated_drawer.dart';
 import 'package:mooltik/editor/reel/timeline.dart';
 import 'package:mooltik/editor/reel/widgets/playhead.dart';
