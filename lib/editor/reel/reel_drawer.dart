c81ddd4ccb7eac8ccfeb90a07182c11c9f9787da
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-05 15:05:43 +0800

    Remove unused import

--- lib/editor/reel/reel_drawer.dart
+++ lib/editor/reel/reel_drawer.dart
@@ -4,7 +4,6 @@ import 'package:flutter_portal/flutter_portal.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mooltik/editor/drawer/animated_drawer.dart';
 import 'package:mooltik/common/app_icon_button.dart';
-import 'package:mooltik/editor/frame/frame_model.dart';
 import 'package:mooltik/editor/frame/frame_thumbnail.dart';
 import 'package:mooltik/editor/reel/widgets/reel_context_menu.dart';
 import 'package:provider/provider.dart';
