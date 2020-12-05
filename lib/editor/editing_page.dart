81df31cfa049a6cddd9a707de6636a24b0dcdb7a
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-05 15:55:19 +0800

    Save on exit

--- lib/editor/editing_page.dart
+++ lib/editor/editing_page.dart
@@ -5,6 +5,7 @@ import 'package:mooltik/editor/drawing_page.dart';
 import 'package:mooltik/editor/frame/frame_thumbnail.dart';
 import 'package:mooltik/editor/reel/timeline.dart';
 import 'package:mooltik/editor/toolbox/toolbox_model.dart';
+import 'package:mooltik/home/project.dart';
 import 'package:provider/provider.dart';
 
 import 'reel/reel_model.dart';
@@ -60,7 +61,10 @@ class EditingPage extends StatelessWidget {
   AppIconButton _buildExitButton(BuildContext context) {
     return AppIconButton(
       icon: FontAwesomeIcons.arrowLeft,
-      onTap: () {
+      onTap: () async {
+        final project = context.read<Project>();
+        await project.save();
+        project.close();
         Navigator.pop(context);
       },
     );
