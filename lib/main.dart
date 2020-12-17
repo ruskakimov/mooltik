1bb8c77c07c30685576e82686a62a97abf991fce
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-17 20:11:00 +0800

    Remove portal dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_portal/flutter_portal.dart';
 import 'package:mooltik/home/home_page.dart';
 
 void main() {
@@ -20,40 +19,38 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Portal(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Mooltik',
-        theme: ThemeData(
-          // Overscroll
-          accentColor: Colors.amber,
-          // Splash
-          highlightColor: Colors.white.withOpacity(0.2),
-          colorScheme: ColorScheme(
-            brightness: Brightness.dark,
-            // Primary
-            primary: Colors.amber,
-            onPrimary: Colors.grey[900],
-            primaryVariant: Colors.amberAccent,
-            // Secondary
-            secondary: Colors.grey[600],
-            onSecondary: Colors.white,
-            secondaryVariant: Colors.grey[800],
-            // Surface
-            surface: Colors.grey[850],
-            onSurface: Colors.grey[100],
-            // Background
-            background: Colors.grey[900],
-            onBackground: Colors.grey[100],
-            // Error
-            error: Colors.redAccent,
-            onError: Colors.white,
-          ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mooltik',
+      theme: ThemeData(
+        // Overscroll
+        accentColor: Colors.amber,
+        // Splash
+        highlightColor: Colors.white.withOpacity(0.2),
+        colorScheme: ColorScheme(
+          brightness: Brightness.dark,
+          // Primary
+          primary: Colors.amber,
+          onPrimary: Colors.grey[900],
+          primaryVariant: Colors.amberAccent,
+          // Secondary
+          secondary: Colors.grey[600],
+          onSecondary: Colors.white,
+          secondaryVariant: Colors.grey[800],
+          // Surface
+          surface: Colors.grey[850],
+          onSurface: Colors.grey[100],
+          // Background
+          background: Colors.grey[900],
+          onBackground: Colors.grey[100],
+          // Error
+          error: Colors.redAccent,
+          onError: Colors.white,
         ),
-        routes: {
-          Navigator.defaultRouteName: (context) => HomePage(),
-        },
       ),
+      routes: {
+        Navigator.defaultRouteName: (context) => HomePage(),
+      },
     );
   }
 }
