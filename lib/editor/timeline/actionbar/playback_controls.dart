e38470dfbde9d56f6fd35f5a660d18b52adc2640
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-18 21:22:19 +0800

    Extract playback controls

---
+++ lib/editor/timeline/actionbar/playback_controls.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mooltik/common/app_icon_button.dart';
+import 'package:mooltik/editor/timeline/actionbar/step_backward_button.dart';
+import 'package:mooltik/editor/timeline/actionbar/step_forward_button.dart';
+import 'package:mooltik/editor/timeline/timeline_model.dart';
+import 'package:provider/provider.dart';
+
+class PlaybackControls extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final timeline = context.watch<TimelineModel>();
+
+    return Row(
+      children: [
+        StepBackwardButton(),
+        AppIconButton(
+          icon: timeline.playing
+              ? FontAwesomeIcons.pause
+              : timeline.playheadPosition == timeline.totalDuration
+                  ? FontAwesomeIcons.undoAlt
+                  : FontAwesomeIcons.play,
+          onTap: timeline.playing
+              ? timeline.pause
+              : timeline.playheadPosition == timeline.totalDuration
+                  ? timeline.replay
+                  : timeline.play,
+        ),
+        StepForwardButton(),
+      ],
+    );
+  }
+}
--- lib/editor/timeline/actionbar/timeline_actionbar.dart
+++ lib/editor/timeline/actionbar/timeline_actionbar.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mooltik/editor/timeline/actionbar/playback_controls.dart';
 import 'package:mooltik/editor/timeline/actionbar/record_button.dart';
-import 'package:mooltik/editor/timeline/actionbar/step_backward_button.dart';
-import 'package:mooltik/editor/timeline/actionbar/step_forward_button.dart';
-import 'package:provider/provider.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:mooltik/common/app_icon_button.dart';
 import 'package:mooltik/editor/timeline/actionbar/time_label.dart';
-import 'package:mooltik/editor/timeline/timeline_model.dart';
 
 class TimelineActionbar extends StatelessWidget {
   const TimelineActionbar({
@@ -15,8 +10,6 @@ class TimelineActionbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final timeline = context.watch<TimelineModel>();
-
     return SizedBox(
       height: 48,
       child: Row(
@@ -26,20 +19,7 @@ class TimelineActionbar extends StatelessWidget {
           Spacer(),
           RecordButton(),
           Spacer(),
-          StepBackwardButton(),
-          AppIconButton(
-            icon: timeline.playing
-                ? FontAwesomeIcons.pause
-                : timeline.playheadPosition == timeline.totalDuration
-                    ? FontAwesomeIcons.undoAlt
-                    : FontAwesomeIcons.play,
-            onTap: timeline.playing
-                ? timeline.pause
-                : timeline.playheadPosition == timeline.totalDuration
-                    ? timeline.replay
-                    : timeline.play,
-          ),
-          StepForwardButton(),
+          PlaybackControls(),
         ],
       ),
     );
