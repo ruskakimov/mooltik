a7bb440557bf543e5cbced53ded9e30e542d2f2c
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-19 00:04:01 +0800

    Init recorder

--- lib/editor/timeline/player_model.dart
+++ lib/editor/timeline/player_model.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sound_lite/flutter_sound.dart';
+import 'package:flutter_sound_platform_interface/flutter_sound_recorder_platform_interface.dart';
 import 'package:mooltik/editor/sound_bite.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class PlayerModel extends ChangeNotifier {
-  bool get isRecording => _isRecording;
-  bool _isRecording = false;
+  FlutterSoundRecorder _recorder;
+
+  bool get isRecording => _recorder?.isRecording ?? false;
 
   SoundBite get soundBite => _soundBite;
   SoundBite _soundBite = SoundBite(
+    file: null,
     startTime: Duration(seconds: 1),
     duration: Duration(seconds: 2),
   );
 
-  void startRecording() {
-    _isRecording = true;
+  Future<void> initRecorder() async {
+    final permit = await Permission.microphone.request();
+    if (permit != PermissionStatus.granted) return;
+    _recorder = FlutterSoundRecorder();
+    await _recorder.openAudioSession();
+  }
+
+  Future<void> startRecording() async {
+    if (_recorder == null) {
+      await initRecorder();
+      if (_recorder == null) return;
+    }
+    // await _recorder.startRecorder(
+    //   toFile: _soundBite.file.path,
+    //   codec: Codec.aacADTS,
+    //   audioSource: AudioSource.voice_communication,
+    // );
     notifyListeners();
   }
 
-  void stopRecording() {
-    _isRecording = false;
+  Future<void> stopRecording() async {
+    await _recorder.stopRecorder();
     notifyListeners();
+  }
+
+  @override
+  Future<void> dispose() async {
+    super.dispose();
+    await _recorder.closeAudioSession();
   }
 }
