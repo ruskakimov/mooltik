7d6f205ab34cb7a3932fc48cd85efe2118f37533
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-18 22:32:39 +0800

    Hook up record button to player model

--- lib/editor/timeline/actionbar/record_button.dart
+++ lib/editor/timeline/actionbar/record_button.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mooltik/common/app_icon_button.dart';
+import 'package:mooltik/editor/timeline/player_model.dart';
 
 class RecordButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final player = context.watch<PlayerModel>();
+
     return AppIconButton(
-      // selected: recording,
+      selected: player.recording,
       icon: FontAwesomeIcons.microphone,
-      onTap: () {},
+      onTap: player.recording ? player.stopRecording : player.startRecording,
     );
   }
 }
--- lib/editor/timeline/player_model.dart
+++ lib/editor/timeline/player_model.dart
@@ -3,4 +3,14 @@ import 'package:flutter/material.dart';
 class PlayerModel extends ChangeNotifier {
   bool get recording => _recording;
   bool _recording = false;
+
+  void startRecording() {
+    _recording = true;
+    notifyListeners();
+  }
+
+  void stopRecording() {
+    _recording = false;
+    notifyListeners();
+  }
 }
