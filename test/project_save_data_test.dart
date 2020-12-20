bcfa626557d32a3525832e424ffb59016f9c3605
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-20 15:43:45 +0800

    Test save path

--- test/project_save_data_test.dart
+++ test/project_save_data_test.dart
@@ -13,12 +13,12 @@ void main() {
       );
       expect(
         jsonEncode(data),
-        '{"width":200.0,"height":100.0,"frames":[{"id":0,"duration":"0:00:03.000000"}],"sounds":[{"id":12,"start_time":"0:00:01.000000","duration":"0:00:02.000000"}]}',
+        '{"width":200.0,"height":100.0,"frames":[{"id":0,"duration":"0:00:03.000000"}],"sounds":[{"path":"","start_time":"0:00:01.000000","duration":"0:00:02.000000"}]}',
       );
     });
     test('decode and encode back', () {
       final json =
-          '{"width":200.0,"height":100.0,"frames":[{"id":0,"duration":"0:00:03.000000"}],"sounds":[{"id":12,"start_time":"0:00:01.000000","duration":"0:00:02.000000"}]}';
+          '{"width":200.0,"height":100.0,"frames":[{"id":0,"duration":"0:00:03.000000"}],"sounds":[{"path":"","start_time":"0:00:01.000000","duration":"0:00:02.000000"}]}';
       final data = ProjectSaveData.fromJson(jsonDecode(json));
       expect(jsonEncode(data), json);
     });
