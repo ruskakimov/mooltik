6a7240d18b43eef099987b0371616088fd552e14
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-22 22:29:59 +0800

    Dummy menu

--- lib/editor/timeline/actionbar/timeline_actionbar.dart
+++ lib/editor/timeline/actionbar/timeline_actionbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_portal/flutter_portal.dart';
 import 'package:mooltik/editor/timeline/actionbar/playback_controls.dart';
 import 'package:mooltik/editor/timeline/actionbar/record_button.dart';
 import 'package:mooltik/editor/timeline/actionbar/time_label.dart';
@@ -18,7 +19,21 @@ class TimelineActionbar extends StatelessWidget {
           Expanded(
             child: TimeLabel(),
           ),
-          RecordButton(),
+          PortalEntry(
+            visible: true,
+            childAnchor: Alignment.center,
+            portalAnchor: Alignment.center,
+            portal: Material(
+              borderRadius: BorderRadius.circular(8),
+              elevation: 10,
+              color: Theme.of(context).colorScheme.primary,
+              child: Container(
+                width: 200,
+                height: 56,
+              ),
+            ),
+            child: RecordButton(),
+          ),
           Expanded(
             child: PlaybackControls(),
           ),
