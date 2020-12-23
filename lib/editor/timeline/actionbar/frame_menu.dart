7e80514c2b9a1b6f440d0fb9583e55251bf4d1a4
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-23 22:16:16 +0800

    Splash

--- lib/editor/timeline/actionbar/frame_menu.dart
+++ lib/editor/timeline/actionbar/frame_menu.dart
@@ -9,6 +9,7 @@ class FrameMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+      clipBehavior: Clip.antiAlias,
       borderRadius: BorderRadius.circular(8),
       elevation: 10,
       color: Theme.of(context).colorScheme.primary,
@@ -48,28 +49,34 @@ class LabeledIconButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 56,
-      height: 56,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Icon(
-            icon,
-            size: 18,
-            color: color,
-          ),
-          SizedBox(height: 8),
-          Text(
-            label,
-            style: TextStyle(
-              fontSize: 10,
-              fontWeight: FontWeight.w700,
+    return InkResponse(
+      highlightColor: Colors.white,
+      splashColor: Colors.white,
+      radius: 56,
+      onTap: () {},
+      child: SizedBox(
+        width: 56,
+        height: 56,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 18,
               color: color,
             ),
-          ),
-        ],
+            SizedBox(height: 8),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.w700,
+                color: color,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
