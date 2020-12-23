cd6cce9109e5a20cc800f1fd3c66fde18a7bebe0
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-23 21:34:25 +0800

    butt widet

--- lib/editor/timeline/actionbar/timeline_actionbar.dart
+++ lib/editor/timeline/actionbar/timeline_actionbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_portal/flutter_portal.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mooltik/editor/timeline/actionbar/playback_controls.dart';
 import 'package:mooltik/editor/timeline/actionbar/record_button.dart';
 import 'package:mooltik/editor/timeline/actionbar/time_label.dart';
@@ -49,7 +50,25 @@ class FrameMenu extends StatelessWidget {
       child: Container(
         width: 200,
         height: 56,
+        child: LabeledIconButton(),
       ),
+    );
+  }
+}
+
+class LabeledIconButton extends StatelessWidget {
+  const LabeledIconButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Icon(FontAwesomeIcons.trashAlt),
+        SizedBox(height: 4),
+        Text('Delete'),
+      ],
     );
   }
 }
