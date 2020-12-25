a2e9a3052655434c0b79e3b36d17caa792a6c6de
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-25 14:48:47 +0800

    Extract timeline view model state

--- lib/editor/timeline/scrollable/timeline_view.dart
+++ lib/editor/timeline/scrollable/timeline_view.dart
@@ -1,53 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:mooltik/editor/sound_clip.dart';
 import 'package:mooltik/editor/timeline/player_model.dart';
-import 'package:mooltik/editor/timeline/scrollable/convert.dart';
 import 'package:mooltik/editor/timeline/scrollable/timeline_painter.dart';
+import 'package:mooltik/editor/timeline/scrollable/timeline_view_model.dart';
 import 'package:mooltik/editor/timeline/timeline_model.dart';
 import 'package:provider/provider.dart';
 
-class TimelineView extends StatefulWidget {
+class TimelineView extends StatelessWidget {
   const TimelineView({Key key}) : super(key: key);
-
-  @override
-  _TimelineViewState createState() => _TimelineViewState();
-}
-
-class _TimelineViewState extends State<TimelineView> {
-  double msPerPx = 10;
-  double _prevMsPerPx = 10;
-  double _scaleOffset;
-  Offset _prevFocalPoint;
 
   @override
   Widget build(BuildContext context) {
     final timeline = context.watch<TimelineModel>();
-    final timelineWidth = durationToPx(timeline.totalDuration, msPerPx);
+    final timelineView = context.watch<TimelineViewModel>();
 
     return GestureDetector(
-      onScaleStart: (ScaleStartDetails details) {
-        _prevMsPerPx = msPerPx;
-        _prevFocalPoint = details.localFocalPoint;
-      },
-      onScaleUpdate: (ScaleUpdateDetails details) {
-        _scaleOffset ??= 1 - details.scale;
-        setState(() {
-          msPerPx = _prevMsPerPx / (details.scale + _scaleOffset);
-        });
-
-        final diff = (details.localFocalPoint - _prevFocalPoint);
-        timeline.scrub(-diff.dx / timelineWidth);
-        _prevFocalPoint = details.localFocalPoint;
-      },
-      onScaleEnd: (ScaleEndDetails details) {
-        _scaleOffset = null;
-      },
-      onTapUp: (TapUpDetails details) {
-        print(details.localPosition);
-        // details.localPosition.dy < frameSliverTop
-        // details.localPosition.dy > frameSliverBottom
-        // iterate visibleFrameSlivers
-      },
+      onScaleStart: timelineView.onScaleStart,
+      onScaleUpdate: timelineView.onScaleUpdate,
+      onScaleEnd: timelineView.onScaleEnd,
+      onTapUp: timelineView.onTapUp,
       child: SizedBox.expand(
         child: ColoredBox(
           color: Colors.black.withOpacity(0.2),
@@ -56,7 +27,7 @@ class _TimelineViewState extends State<TimelineView> {
               frames: timeline.frames,
               selectedFrameIndex: timeline.selectedFrameIndex,
               selectedFrameStartTime: timeline.selectedFrameStartTime,
-              msPerPx: msPerPx,
+              msPerPx: timelineView.msPerPx,
               playheadPosition: timeline.playheadPosition,
               soundBite: context.select<PlayerModel, SoundClip>(
                 (PlayerModel player) => player.soundClips.isNotEmpty
--- lib/editor/timeline/scrollable/timeline_view_model.dart
+++ lib/editor/timeline/scrollable/timeline_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mooltik/editor/timeline/scrollable/convert.dart';
 import 'package:mooltik/editor/timeline/timeline_model.dart';
 
 class TimelineViewModel extends ChangeNotifier {
@@ -7,4 +8,40 @@ class TimelineViewModel extends ChangeNotifier {
   }) : _timeline = timeline;
 
   final TimelineModel _timeline;
+
+  double get msPerPx => _msPerPx;
+  double _msPerPx = 10;
+  double _prevMsPerPx = 10;
+  double _scaleOffset;
+  Offset _prevFocalPoint;
+
+  double get timelineWidth => durationToPx(_timeline.totalDuration, _msPerPx);
+
+  void onScaleStart(ScaleStartDetails details) {
+    _prevMsPerPx = _msPerPx;
+    _prevFocalPoint = details.localFocalPoint;
+  }
+
+  void onScaleUpdate(ScaleUpdateDetails details) {
+    _scaleOffset ??= 1 - details.scale;
+    _msPerPx = _prevMsPerPx / (details.scale + _scaleOffset);
+
+    final diff = (details.localFocalPoint - _prevFocalPoint);
+    _timeline.scrub(-diff.dx / timelineWidth);
+
+    _prevFocalPoint = details.localFocalPoint;
+
+    notifyListeners();
+  }
+
+  void onScaleEnd(ScaleEndDetails details) {
+    _scaleOffset = null;
+  }
+
+  void onTapUp(TapUpDetails details) {
+    print(details.localPosition);
+    // details.localPosition.dy < frameSliverTop
+    // details.localPosition.dy > frameSliverBottom
+    // iterate visibleFrameSlivers
+  }
 }
