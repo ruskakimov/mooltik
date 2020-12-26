fa0e7fbf84df1b09cb7cf5b128ebe9a2d974b508
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-26 10:19:14 +0800

    Swap mic and play buttons

--- lib/editor/timeline/actionbar/play_button.dart
+++ lib/editor/timeline/actionbar/play_button.dart
@@ -1,26 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mooltik/common/app_icon_button.dart';
-import 'package:mooltik/editor/timeline/actionbar/step_backward_button.dart';
-import 'package:mooltik/editor/timeline/actionbar/step_forward_button.dart';
 import 'package:mooltik/editor/timeline/player_model.dart';
 import 'package:mooltik/editor/timeline/timeline_model.dart';
 import 'package:provider/provider.dart';
 
-class PlaybackControls extends StatelessWidget {
+class PlayButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        StepBackwardButton(),
-        _buildMiddleButton(context),
-        StepForwardButton(),
-      ],
-    );
-  }
-
-  Widget _buildMiddleButton(BuildContext context) {
     final timeline = context.watch<TimelineModel>();
     final player = context.watch<PlayerModel>();
 
--- lib/editor/timeline/actionbar/timeline_actionbar.dart
+++ lib/editor/timeline/actionbar/timeline_actionbar.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mooltik/editor/timeline/actionbar/play_button.dart';
 import 'package:mooltik/editor/timeline/scrollable/timeline_view_model.dart';
 
 import 'package:provider/provider.dart';
 import 'package:flutter_portal/flutter_portal.dart';
 import 'package:mooltik/editor/timeline/actionbar/frame_menu.dart';
-import 'package:mooltik/editor/timeline/actionbar/playback_controls.dart';
 import 'package:mooltik/editor/timeline/actionbar/record_button.dart';
 import 'package:mooltik/editor/timeline/actionbar/time_label.dart';
 
@@ -28,10 +28,15 @@ class TimelineActionbar extends StatelessWidget {
             childAnchor: Alignment.center,
             portalAnchor: Alignment.center,
             portal: FrameMenu(),
-            child: RecordButton(),
+            child: PlayButton(),
           ),
           Expanded(
-            child: PlaybackControls(),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                RecordButton(),
+              ],
+            ),
           ),
         ],
       ),
