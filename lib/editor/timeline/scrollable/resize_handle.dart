251d26dc8145ec951b1f232ae45fb4e7e5a76cf6
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-28 20:11:39 +0800

    Extract resize handle file

---
+++ lib/editor/timeline/scrollable/resize_handle.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:mooltik/editor/timeline/scrollable/timeline_view_model.dart';
+import 'package:mooltik/editor/timeline/timeline_model.dart';
+
+class PositionedResizeHandle extends StatelessWidget {
+  final double width = 24;
+  final double height = 48;
+
+  @override
+  Widget build(BuildContext context) {
+    final timelineView = context.watch<TimelineViewModel>();
+    final timeline = context.watch<TimelineModel>();
+
+    final frameSliverMid = timelineView.frameSliverTop +
+        (timelineView.frameSliverBottom - timelineView.frameSliverTop) / 2;
+
+    return Positioned(
+      left: timelineView.xFromTime(timeline.selectedFrameEndTime) - width / 2,
+      top: frameSliverMid - height / 2,
+      child: ResizeHandle(
+        width: width,
+        height: height,
+      ),
+    );
+  }
+}
+
+class ResizeHandle extends StatelessWidget {
+  const ResizeHandle({
+    Key key,
+    this.width,
+    this.height,
+  }) : super(key: key);
+
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      borderRadius: BorderRadius.circular(8),
+      color: Theme.of(context).colorScheme.primary,
+      elevation: 10,
+      child: SizedBox(
+        width: width,
+        height: height,
+        child: RotatedBox(
+          quarterTurns: 1,
+          child: Icon(
+            Icons.drag_handle_rounded,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/editor/timeline/timeline_panel.dart
+++ lib/editor/timeline/timeline_panel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mooltik/editor/timeline/player_model.dart';
+import 'package:mooltik/editor/timeline/scrollable/resize_handle.dart';
 import 'package:mooltik/editor/timeline/scrollable/timeline_view_model.dart';
 import 'package:mooltik/editor/timeline/timeline_model.dart';
 import 'package:mooltik/home/project.dart';
@@ -46,60 +47,6 @@ class TimelinePanel extends StatelessWidget {
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class PositionedResizeHandle extends StatelessWidget {
-  final double width = 24;
-  final double height = 48;
-
-  @override
-  Widget build(BuildContext context) {
-    final timelineView = context.watch<TimelineViewModel>();
-    final timeline = context.watch<TimelineModel>();
-
-    final frameSliverMid = timelineView.frameSliverTop +
-        (timelineView.frameSliverBottom - timelineView.frameSliverTop) / 2;
-
-    return Positioned(
-      left: timelineView.xFromTime(timeline.selectedFrameEndTime) - width / 2,
-      top: frameSliverMid - height / 2,
-      child: ResizeHandle(
-        width: width,
-        height: height,
-      ),
-    );
-  }
-}
-
-class ResizeHandle extends StatelessWidget {
-  const ResizeHandle({
-    Key key,
-    this.width,
-    this.height,
-  }) : super(key: key);
-
-  final double width;
-  final double height;
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      borderRadius: BorderRadius.circular(8),
-      color: Theme.of(context).colorScheme.primary,
-      elevation: 10,
-      child: SizedBox(
-        width: width,
-        height: height,
-        child: RotatedBox(
-          quarterTurns: 1,
-          child: Icon(
-            Icons.drag_handle_rounded,
-            color: Theme.of(context).colorScheme.onPrimary,
-          ),
         ),
       ),
     );
