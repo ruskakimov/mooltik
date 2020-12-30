c4b96ab12cd50bdd66b0e34aa2c7059ca0a63ca6
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-31 00:24:08 +0800

    Progress indicator

--- lib/editor/export_dialog.dart
+++ lib/editor/export_dialog.dart
@@ -19,29 +19,38 @@ class ExportDialog extends StatelessWidget {
               width: 260,
               height: 260,
               child: CircularProgressIndicator(
-                value: 0,
+                value: 0.72,
+                backgroundColor: Colors.black12,
                 strokeWidth: 20,
               ),
             ),
-            RaisedButton(
-              color: Theme.of(context).colorScheme.primary,
-              highlightColor: Colors.white12,
-              splashColor: Colors.white,
-              shape: CircleBorder(),
-              onPressed: () {},
-              child: SizedBox(
-                width: 230,
-                height: 230,
-                child: Center(
-                  child: Text(
-                    'Export video',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).colorScheme.onPrimary,
-                    ),
-                  ),
-                ),
+            // RaisedButton(
+            //   color: Theme.of(context).colorScheme.primary,
+            //   highlightColor: Colors.white12,
+            //   splashColor: Colors.white,
+            //   shape: CircleBorder(),
+            //   onPressed: () {},
+            //   child: SizedBox(
+            //     width: 230,
+            //     height: 230,
+            //     child: Center(
+            //       child: Text(
+            //         'Export video',
+            //         style: TextStyle(
+            //           fontSize: 24,
+            //           fontWeight: FontWeight.bold,
+            //           color: Theme.of(context).colorScheme.onPrimary,
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            Text(
+              '72%',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
             ),
           ],
