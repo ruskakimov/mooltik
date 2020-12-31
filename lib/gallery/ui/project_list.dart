730be2e0ddf25fdb25c6cbccc7c5f583920726a3
Rustem Kakimov <ruskakimov@gmail.com>
2020-12-31 21:07:29 +0800

    Extract project thumbnail file

--- lib/gallery/ui/project_list.dart
+++ lib/gallery/ui/project_list.dart
@@ -1,7 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:mooltik/common/data/project/project.dart';
+import 'package:mooltik/gallery/ui/project_thumbnail.dart';
 import 'package:provider/provider.dart';
 import 'package:mooltik/editing/editing_page.dart';
 import 'package:mooltik/gallery/data/gallery_model.dart';
@@ -45,33 +44,6 @@ class ProjectList extends StatelessWidget {
         );
       },
       separatorBuilder: (context, index) => const SizedBox(width: 24),
-    );
-  }
-}
-
-class ProjectThumbnail extends StatelessWidget {
-  const ProjectThumbnail({
-    Key key,
-    @required this.thumbnail,
-  }) : super(key: key);
-
-  final File thumbnail;
-
-  @override
-  Widget build(BuildContext context) {
-    context.watch<Project>();
-
-    return Container(
-      width: 200,
-      height: 200,
-      color: Colors.white,
-      child: thumbnail.existsSync()
-          ? Image.memory(
-              // Temporary fix for this issue https://github.com/flutter/flutter/issues/17419
-              thumbnail.readAsBytesSync(),
-              fit: BoxFit.cover,
-            )
-          : null,
     );
   }
 }
---
+++ lib/gallery/ui/project_thumbnail.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:mooltik/common/data/project/project.dart';
+import 'package:provider/provider.dart';
+
+class ProjectThumbnail extends StatelessWidget {
+  const ProjectThumbnail({
+    Key key,
+    @required this.thumbnail,
+  }) : super(key: key);
+
+  final File thumbnail;
+
+  @override
+  Widget build(BuildContext context) {
+    context.watch<Project>();
+
+    return Container(
+      width: 200,
+      height: 200,
+      color: Colors.white,
+      child: thumbnail.existsSync()
+          ? Image.memory(
+              // Temporary fix for this issue https://github.com/flutter/flutter/issues/17419
+              thumbnail.readAsBytesSync(),
+              fit: BoxFit.cover,
+            )
+          : null,
+    );
+  }
+}
