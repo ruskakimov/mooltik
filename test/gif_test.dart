f338348b223b57ac71c691d90cd6bd3a4f7ab80c
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-01 20:36:13 +0800

    Remove old test

--- test/gif_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:mooltik/common/data/io/gif.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('Throws exception if no frames are given', () {
-    expect(() => makeGif([]), throwsArgumentError);
-  });
-}
