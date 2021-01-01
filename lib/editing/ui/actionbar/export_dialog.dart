57a2c0fb081e027d0a35ea869de757ef6dc0e263
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-01 20:47:41 +0800

    Extract export button widget

--- lib/editing/ui/actionbar/export_dialog.dart
+++ lib/editing/ui/actionbar/export_dialog.dart
@@ -25,41 +25,16 @@ class ExportDialog extends StatelessWidget {
         child: Stack(
           alignment: Alignment.center,
           children: [
-            SizedBox(
-              width: sideWidth - loadingStrokeWidth,
-              height: sideWidth - loadingStrokeWidth,
-              child: CircularProgressIndicator(
-                value: 0,
-                backgroundColor: Colors.black12,
-                strokeWidth: loadingStrokeWidth,
-              ),
-            ),
-            RaisedButton(
-              color: Theme.of(context).colorScheme.primary,
-              highlightColor: Colors.white12,
-              splashColor: Colors.white,
-              shape: CircleBorder(),
-              onPressed: () {
+            _buildLoadingIndicator(),
+            _ExportButton(
+              diameter: sideWidth - loadingStrokeWidth * 2,
+              onTap: () {
                 // get temp directory
                 // timeline.frames -> images
                 // write PNGs (cannot use project pngs, cos they have transparent bg)
                 // mp4Write(slides, file, temp)
                 // await ImageGallerySaver.saveFile(video.path);
               },
-              child: SizedBox(
-                width: sideWidth - loadingStrokeWidth * 2,
-                height: sideWidth - loadingStrokeWidth * 2,
-                child: Center(
-                  child: Text(
-                    'Export video',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).colorScheme.onPrimary,
-                    ),
-                  ),
-                ),
-              ),
             ),
             // Text(
             //   '72%',
@@ -70,6 +45,54 @@ class ExportDialog extends StatelessWidget {
             //   ),
             // ),
           ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox _buildLoadingIndicator() {
+    return SizedBox(
+      width: sideWidth - loadingStrokeWidth,
+      height: sideWidth - loadingStrokeWidth,
+      child: CircularProgressIndicator(
+        value: 0,
+        backgroundColor: Colors.black12,
+        strokeWidth: loadingStrokeWidth,
+      ),
+    );
+  }
+}
+
+class _ExportButton extends StatelessWidget {
+  const _ExportButton({
+    Key key,
+    @required this.diameter,
+    this.onTap,
+  }) : super(key: key);
+
+  final double diameter;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      color: Theme.of(context).colorScheme.primary,
+      highlightColor: Colors.white12,
+      splashColor: Colors.white,
+      shape: CircleBorder(),
+      onPressed: onTap,
+      child: SizedBox(
+        width: diameter,
+        height: diameter,
+        child: Center(
+          child: Text(
+            'Export video',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+          ),
         ),
       ),
     );
