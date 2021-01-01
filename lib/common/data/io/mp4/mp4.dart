f3a34f30db2f617b043d64e6542be1399742dfcb
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-01 16:51:26 +0800

    Write mp4

--- lib/common/data/io/mp4/mp4.dart
+++ lib/common/data/io/mp4/mp4.dart
@@ -1,9 +1,20 @@
 import 'dart:io';
+import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
+import 'package:path/path.dart' as p;
 import 'package:mooltik/common/data/io/mp4/slide.dart';
 import 'package:mooltik/common/data/io/mp4/slide_concat_demuxer.dart';
 
-Future<void> mp4Write(File mp4File, List<Slide> slides) {
+Future<void> mp4Write(
+  File mp4File,
+  List<Slide> slides,
+  Directory tempDir,
+) async {
   assert(slides != null && slides.isNotEmpty);
 
-  generateSlideConcatDemuxer(slides);
+  final concatFile = File(p.join(tempDir.path, 'concat.txt'));
+  await concatFile.writeAsString(generateSlideConcatDemuxer(slides));
+
+  await FlutterFFmpeg().execute(
+    '-y -f concat -safe 0 -i ${concatFile.path} -vf fps=24 -pix_fmt yuv420p ${mp4File.path}',
+  );
 }
