6e9ce9b7f19900b16e26ddbc230a02d14a847140
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-01 16:42:11 +0800

    Extract concat demuxer generator

--- lib/common/data/io/mp4/mp4.dart
+++ lib/common/data/io/mp4/mp4.dart
@@ -1,28 +1,9 @@
 import 'dart:io';
 import 'package:mooltik/common/data/io/mp4/slide.dart';
+import 'package:mooltik/common/data/io/mp4/slide_concat_demuxer.dart';
 
 Future<void> mp4Write(File mp4File, List<Slide> slides) {
   assert(slides != null && slides.isNotEmpty);
 
-  _getConcatDemuxer(slides);
+  generateSlideConcatDemuxer(slides);
 }
-
-String _getConcatDemuxer(List<Slide> slides) {
-  String concatDemuxer = '';
-  for (final slide in slides) {
-    concatDemuxer += '''
-      file '${slide.pngImage.path}'
-      duration ${_formatDuration(slide.duration)}
-    ''';
-  }
-
-  // Due to a quirk, the last image has to be specified twice - the 2nd time without any duration directive.
-  // Source: https://trac.ffmpeg.org/wiki/Slideshow
-  concatDemuxer += '''
-    file '${slides.last.pngImage.path}'
-  ''';
-
-  return concatDemuxer;
-}
-
-String _formatDuration(Duration duration) => '${duration.inMilliseconds}ms';
---
+++ lib/common/data/io/mp4/slide_concat_demuxer.dart
@@ -0,0 +1,21 @@
+import 'package:mooltik/common/data/io/mp4/slide.dart';
+
+String generateSlideConcatDemuxer(List<Slide> slides) {
+  String concatDemuxer = '';
+  for (final slide in slides) {
+    concatDemuxer += '''
+      file '${slide.pngImage.path}'
+      duration ${_formatDuration(slide.duration)}
+    ''';
+  }
+
+  // Due to a quirk, the last image has to be specified twice - the 2nd time without any duration directive.
+  // Source: https://trac.ffmpeg.org/wiki/Slideshow
+  concatDemuxer += '''
+    file '${slides.last.pngImage.path}'
+  ''';
+
+  return concatDemuxer;
+}
+
+String _formatDuration(Duration duration) => '${duration.inMilliseconds}ms';
