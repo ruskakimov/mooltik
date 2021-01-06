addb56cac5dade37afcd1542ac06f6f7c7672c37
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-06 18:42:28 +0800

    Remove unnecessary calc

--- lib/drawing/data/easel_model.dart
+++ lib/drawing/data/easel_model.dart
@@ -83,7 +83,7 @@ class EaselModel extends ChangeNotifier {
     _scale = (_screenSize.height - topPadding) / frameSize.height;
     _offset = Offset(
       (_screenSize.width - frameSize.width * _scale) / 2,
-      (topPadding + _screenSize.height - frameSize.height * _scale) / 2,
+      topPadding,
     );
     _rotation = 0;
   }
