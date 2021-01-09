1fed5ade59167c6a58fc9bd012e209f542897999
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-09 15:49:27 +0800

    Generate image util

---
+++ lib/common/data/io/generate_image.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart' show CustomPainter;
+
+Future<Image> generateImage(
+  CustomPainter painter,
+  int width,
+  int height,
+) async {
+  final recorder = PictureRecorder();
+  final canvas = Canvas(recorder);
+  painter.paint(canvas, Size(width.toDouble(), height.toDouble()));
+  final picture = recorder.endRecording();
+  return picture.toImage(width, height);
+}
