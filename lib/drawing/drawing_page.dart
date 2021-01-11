465e143a06446ed040878171b7dad8a1ac68c26d
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-11 13:49:57 +0800

    Size picker ui

--- lib/drawing/drawing_page.dart
+++ lib/drawing/drawing_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mooltik/common/ui/surface.dart';
 import 'package:mooltik/drawing/data/frame/frame_model.dart';
 import 'package:mooltik/drawing/ui/drawing_actionbar.dart';
 import 'package:mooltik/drawing/data/onion_model.dart';
@@ -51,16 +50,9 @@ class DrawingPage extends StatelessWidget {
                         ),
                         Align(
                           alignment: Alignment.centerLeft,
-                          child: Container(
-                            width: 52,
-                            height: 112,
-                            decoration: BoxDecoration(
-                              color: Colors.black26,
-                              borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(16),
-                                bottomRight: Radius.circular(16),
-                              ),
-                            ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: SizePicker(),
                           ),
                         ),
                       ],
@@ -72,6 +64,35 @@ class DrawingPage extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class SizePicker extends StatelessWidget {
+  const SizePicker({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        color: Color(0xC4C4C4).withOpacity(0.5),
+        shape: BoxShape.circle,
+      ),
+      child: Center(
+        child: Container(
+          width: 30,
+          height: 30,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.white),
+          ),
+        ),
+      ),
     );
   }
 }
