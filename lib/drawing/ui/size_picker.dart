afe9e12910f067b82e18ad6e82b4bcdae6ec787b
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-11 15:10:49 +0800

    Extract circle widget

--- lib/drawing/ui/size_picker.dart
+++ lib/drawing/ui/size_picker.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:mooltik/drawing/data/toolbox/toolbox_model.dart';
 
 class SizePicker extends StatelessWidget {
   const SizePicker({
@@ -7,24 +9,47 @@ class SizePicker extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 60,
-      height: 60,
-      decoration: BoxDecoration(
-        color: Color(0xC4C4C4).withOpacity(0.5),
-        shape: BoxShape.circle,
-      ),
+    final toolbox = context.watch<ToolboxModel>();
+
+    return _Circle(
+      radius: 60,
+      color: Color(0xC4C4C4).withOpacity(0.5),
       child: Center(
-        child: Container(
-          width: 30,
-          height: 30,
-          decoration: BoxDecoration(
-            color: Colors.black,
-            shape: BoxShape.circle,
-            border: Border.all(color: Colors.white),
-          ),
+        child: _Circle(
+          radius: 30,
+          color: Colors.black,
+          border: Border.all(color: Colors.white),
         ),
       ),
+    );
+  }
+}
+
+class _Circle extends StatelessWidget {
+  const _Circle({
+    Key key,
+    @required this.radius,
+    @required this.color,
+    this.border,
+    this.child,
+  }) : super(key: key);
+
+  final double radius;
+  final Color color;
+  final BoxBorder border;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: radius,
+      height: radius,
+      decoration: BoxDecoration(
+        color: color,
+        shape: BoxShape.circle,
+        border: border,
+      ),
+      child: child,
     );
   }
 }
