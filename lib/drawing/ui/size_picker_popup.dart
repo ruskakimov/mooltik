54ca4c96b7f5488649f60872d81a48ae82499fd9
Rustem Kakimov <ruskakimov@gmail.com>
2021-01-20 21:24:35 +0800

    Add shadow

--- lib/drawing/ui/size_picker_popup.dart
+++ lib/drawing/ui/size_picker_popup.dart
@@ -7,20 +7,22 @@ class SizePickerPopup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 150,
-      height: 52,
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadiusDirectional.circular(8),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Icon(Icons.fiber_manual_record),
-          Icon(Icons.fiber_manual_record),
-          Icon(Icons.fiber_manual_record),
-        ],
+    return Material(
+      color: Theme.of(context).colorScheme.secondary,
+      borderRadius: BorderRadiusDirectional.circular(8),
+      clipBehavior: Clip.antiAlias,
+      elevation: 10,
+      child: SizedBox(
+        width: 150,
+        height: 52,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // Icon(Icons.fiber_manual_record),
+            // Icon(Icons.fiber_manual_record),
+            // Icon(Icons.fiber_manual_record),
+          ],
+        ),
       ),
     );
   }
