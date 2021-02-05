eec927806d9482365c615f3020bd83c172c7e6b7
Rustem Kakimov <ruskakimov@gmail.com>
2021-02-05 23:15:30 +0800

    Add export

--- lib/drawing/data/toolbox/toolbox_model.dart
+++ lib/drawing/data/toolbox/toolbox_model.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mooltik/drawing/data/toolbox/tools/brush.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'tools/tools.dart';
