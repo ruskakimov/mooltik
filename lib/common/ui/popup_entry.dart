2096c3a314a9e37bf3e6203ce1d82d00aa45ffe6
Rustem Kakimov <ruskakimov@gmail.com>
2021-02-06 14:31:25 +0800

    Extract popup entry widget

---
+++ lib/common/ui/popup_entry.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_portal/flutter_portal.dart';
+
+class PopupEntry extends StatelessWidget {
+  const PopupEntry({
+    Key key,
+    @required this.visible,
+    @required this.popup,
+    @required this.child,
+    this.onTapOutside,
+  }) : super(key: key);
+
+  final bool visible;
+  final Widget popup;
+  final Widget child;
+  final VoidCallback onTapOutside;
+
+  @override
+  Widget build(BuildContext context) {
+    return PortalEntry(
+      visible: visible,
+      portal: Listener(
+        behavior: HitTestBehavior.translucent,
+        onPointerDown: (_) {
+          onTapOutside?.call();
+        },
+      ),
+      child: PortalEntry(
+        visible: visible,
+        portal: popup,
+        portalAnchor: Alignment.topCenter,
+        child: IgnorePointer(
+          ignoring: visible,
+          child: child,
+        ),
+        childAnchor: Alignment.bottomCenter.add(Alignment(0, 0.2)),
+      ),
+    );
+  }
+}
