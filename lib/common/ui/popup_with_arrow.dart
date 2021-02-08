e36f3e47635e2400b56af93fa71a0562c27011a7
Rustem Kakimov <ruskakimov@gmail.com>
2021-02-08 18:52:44 +0800

    Rotate arrow

--- lib/common/ui/popup_with_arrow.dart
+++ lib/common/ui/popup_with_arrow.dart
@@ -4,17 +4,26 @@ const double _triangleWidth = 24;
 const double _triangleHeight = 14;
 const double _triangleBorderRadius = 4;
 
+enum ArrowSide {
+  top,
+  left,
+  right,
+  bottom,
+}
+
 class PopupWithArrow extends StatelessWidget {
   const PopupWithArrow({
     Key key,
     @required this.width,
     this.arrowOffset,
     this.child,
+    this.arrowSide = ArrowSide.top,
   }) : super(key: key);
 
   final double width;
   final double arrowOffset;
   final Widget child;
+  final ArrowSide arrowSide;
 
   @override
   Widget build(BuildContext context) {
@@ -36,10 +45,28 @@ class PopupWithArrow extends StatelessWidget {
           left: arrowOffset == null
               ? (width - _triangleWidth) / 2
               : arrowOffset - _triangleWidth / 2,
-          child: _Triangle(),
+          child: RotatedBox(
+            quarterTurns: _triangleQuarterTurns(),
+            child: _Triangle(),
+          ),
         ),
       ],
     );
+  }
+
+  int _triangleQuarterTurns() {
+    switch (arrowSide) {
+      case ArrowSide.top:
+        return 0;
+      case ArrowSide.right:
+        return 1;
+      case ArrowSide.bottom:
+        return 2;
+      case ArrowSide.left:
+        return 3;
+      default:
+        return 0;
+    }
   }
 }
 
