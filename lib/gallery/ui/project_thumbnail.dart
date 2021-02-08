3e83e26d2269e2a67dbc0bcbc15794537f567d0e
Rustem Kakimov <ruskakimov@gmail.com>
2021-02-08 18:11:48 +0800

    Open/close menu on longpress

--- lib/gallery/ui/project_thumbnail.dart
+++ lib/gallery/ui/project_thumbnail.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:mooltik/common/data/project/project.dart';
+import 'package:mooltik/common/ui/popup_entry.dart';
+import 'package:mooltik/common/ui/popup_with_arrow.dart';
 import 'package:provider/provider.dart';
 
-class ProjectThumbnail extends StatelessWidget {
+class ProjectThumbnail extends StatefulWidget {
   const ProjectThumbnail({
     Key key,
     @required this.thumbnail,
@@ -14,24 +16,52 @@ class ProjectThumbnail extends StatelessWidget {
   final VoidCallback onTap;
 
   @override
+  _ProjectThumbnailState createState() => _ProjectThumbnailState();
+}
+
+class _ProjectThumbnailState extends State<ProjectThumbnail> {
+  bool _menuOpen = false;
+
+  @override
   Widget build(BuildContext context) {
     context.watch<Project>();
 
     return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: 320,
-        height: 180,
-        color: Colors.white,
-        child: thumbnail.existsSync()
-            ? Image.memory(
-                // Temporary fix for this issue https://github.com/flutter/flutter/issues/17419
-                thumbnail.readAsBytesSync(),
-                fit: BoxFit.cover,
-                gaplessPlayback: true,
-              )
-            : null,
+      onTap: widget.onTap,
+      onLongPress: () {
+        setState(() => _menuOpen = true);
+      },
+      child: PopupEntry(
+        visible: _menuOpen,
+        popup: _buildProjectMenu(),
+        child: _buildThumbnail(),
+        onTapOutside: () {
+          setState(() => _menuOpen = false);
+        },
       ),
+    );
+  }
+
+  Widget _buildProjectMenu() {
+    return PopupWithArrow(
+      width: 100,
+      child: SizedBox(height: 60),
+    );
+  }
+
+  Widget _buildThumbnail() {
+    return Container(
+      width: 320,
+      height: 180,
+      color: Colors.white,
+      child: widget.thumbnail.existsSync()
+          ? Image.memory(
+              // Temporary fix for this issue https://github.com/flutter/flutter/issues/17419
+              widget.thumbnail.readAsBytesSync(),
+              fit: BoxFit.cover,
+              gaplessPlayback: true,
+            )
+          : null,
     );
   }
 }
