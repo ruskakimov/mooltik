476862af4e4f31fca737865cf2cb7cd1c3a52d20
Rustem Kakimov <ruskakimov@gmail.com>
2021-02-09 13:06:51 +0800

    Open/close trash

--- lib/gallery/ui/trash_button.dart
+++ lib/gallery/ui/trash_button.dart
@@ -3,21 +3,34 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mooltik/common/ui/app_icon_button.dart';
 import 'package:mooltik/common/ui/popup_with_arrow.dart';
 
-class TrashButton extends StatelessWidget {
+class TrashButton extends StatefulWidget {
   const TrashButton({
     Key key,
   }) : super(key: key);
 
   @override
+  _TrashButtonState createState() => _TrashButtonState();
+}
+
+class _TrashButtonState extends State<TrashButton> {
+  bool _trashOpen = false;
+
+  @override
   Widget build(BuildContext context) {
     return PopupWithArrowEntry(
-      visible: false,
+      visible: _trashOpen,
       arrowSide: ArrowSide.top,
       arrowSidePosition: ArrowSidePosition.end,
       popupBody: SizedBox(width: 200, height: 300),
       child: AppIconButton(
         icon: FontAwesomeIcons.trashAlt,
+        onTap: () {
+          setState(() => _trashOpen = true);
+        },
       ),
+      onTapOutside: () {
+        setState(() => _trashOpen = false);
+      },
     );
   }
 }
