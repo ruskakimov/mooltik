5a79f7094b01c35c5321146184e810c97e71ec1a
Rustem Kakimov <ruskakimov@gmail.com>
2021-02-09 12:48:40 +0800

    Trash button in home bar

--- lib/gallery/ui/home_bar.dart
+++ lib/gallery/ui/home_bar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mooltik/common/ui/app_icon_button.dart';
+import 'package:mooltik/common/ui/popup_with_arrow.dart';
 import 'package:mooltik/common/ui/surface.dart';
 
 class HomeBar extends StatelessWidget {
@@ -9,14 +12,49 @@ class HomeBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Surface(
-      child: Center(
-        child: Container(
-          width: 56,
-          height: 56,
-          color: Theme.of(context).colorScheme.primary,
-          child: Image.asset('assets/logo_foreground.png', fit: BoxFit.cover),
-        ),
+      child: Row(
+        children: [
+          _Logo(),
+          Spacer(),
+          _TrashButton(),
+          SizedBox(width: 16),
+        ],
       ),
+    );
+  }
+}
+
+class _TrashButton extends StatelessWidget {
+  const _TrashButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupWithArrowEntry(
+      visible: false,
+      arrowSide: ArrowSide.top,
+      arrowSidePosition: ArrowSidePosition.end,
+      popupBody: SizedBox(width: 200, height: 300),
+      child: AppIconButton(
+        icon: FontAwesomeIcons.trashAlt,
+      ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 56,
+      height: 56,
+      color: Theme.of(context).colorScheme.primary,
+      child: Image.asset('assets/logo_foreground.png', fit: BoxFit.cover),
     );
   }
 }
