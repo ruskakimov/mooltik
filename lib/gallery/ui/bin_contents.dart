c45f7cebb3864cd257895192ecf961eeb64db790
Rustem Kakimov <ruskakimov@gmail.com>
2021-02-12 20:12:55 +0800

    Destroy slidable button

--- lib/gallery/ui/bin_contents.dart
+++ lib/gallery/ui/bin_contents.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mooltik/common/ui/app_icon_button.dart';
+import 'package:mooltik/common/ui/labeled_icon_button.dart';
 import 'package:mooltik/gallery/data/gallery_model.dart';
 import 'package:provider/provider.dart';
 
@@ -17,19 +21,38 @@ class BinContents extends StatelessWidget {
         padding: const EdgeInsets.symmetric(vertical: 6.0),
         children: [
           for (final project in binnedProjects)
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 12.0,
-                vertical: 6.0,
-              ),
-              child: Container(
-                clipBehavior: Clip.antiAlias,
-                decoration: BoxDecoration(
-                  color:
-                      Colors.white, // in case thumbnail is missing background
-                  borderRadius: BorderRadius.circular(8),
+            Slidable(
+              actionPane: SlidableDrawerActionPane(),
+              actionExtentRatio: 0.5,
+              actions: [
+                SlideAction(
+                  child: Material(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+                    clipBehavior: Clip.antiAlias,
+                    child: LabeledIconButton(
+                      icon: FontAwesomeIcons.fireAlt,
+                      label: 'Destroy',
+                      color: Colors.white,
+                      onTap: () {},
+                    ),
+                  ),
+                )
+              ],
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12.0,
+                  vertical: 6.0,
                 ),
-                child: Image.file(project.thumbnail),
+                child: Container(
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                    color:
+                        Colors.white, // in case thumbnail is missing background
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Image.file(project.thumbnail),
+                ),
               ),
             )
         ],
