d36695fbc085039913158db6ae3818f7cc631490
Rustem Kakimov <ruskakimov@gmail.com>
2021-03-12 20:29:01 +0800

    Disable audio import button when playing

--- lib/editing/ui/timeline/actionbar/import_audio_button.dart
+++ lib/editing/ui/timeline/actionbar/import_audio_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mooltik/common/data/project/project.dart';
+import 'package:mooltik/editing/data/timeline_model.dart';
 import 'package:provider/provider.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mooltik/common/ui/app_icon_button.dart';
@@ -16,6 +17,9 @@ class ImportAudioButton extends StatelessWidget {
       create: (context) => ImporterModel(),
       builder: (context, _) {
         final importer = context.watch<ImporterModel>();
+        final playing = context.select<TimelineModel, bool>(
+          (timeline) => timeline.isPlaying,
+        );
 
         if (importer.importing) {
           return SizedBox(
@@ -32,14 +36,16 @@ class ImportAudioButton extends StatelessWidget {
 
         return AppIconButton(
           icon: FontAwesomeIcons.music,
-          onTap: () async {
-            try {
-              final project = context.read<Project>();
-              await importer.importAudioTo(project);
-            } catch (e) {
-              _showFailedImportError(context);
-            }
-          },
+          onTap: playing
+              ? null
+              : () async {
+                  try {
+                    final project = context.read<Project>();
+                    await importer.importAudioTo(project);
+                  } catch (e) {
+                    _showFailedImportError(context);
+                  }
+                },
         );
       },
     );
