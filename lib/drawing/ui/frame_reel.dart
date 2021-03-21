a4d60a30496dfc5863ed51d96511f8df3eb6e0d1
Rustem Kakimov <ruskakimov@gmail.com>
2021-03-21 14:35:45 +0800

    Dummy list

--- lib/drawing/ui/frame_reel.dart
+++ lib/drawing/ui/frame_reel.dart
@@ -5,8 +5,18 @@ class FrameReel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: 20,
+      itemBuilder: (context, index) => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: AspectRatio(
+          aspectRatio: 16 / 9,
+          child: Container(
+            color: Colors.red,
+          ),
+        ),
+      ),
     );
   }
 }
