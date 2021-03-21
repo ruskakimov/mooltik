8abf952cfb7296bd3bbccb0584b5086026761625
Rustem Kakimov <ruskakimov@gmail.com>
2021-03-21 15:44:56 +0800

    Centered list

--- lib/drawing/ui/frame_reel.dart
+++ lib/drawing/ui/frame_reel.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
 
+const _framePadding = const EdgeInsets.only(
+  left: 4.0,
+  right: 4.0,
+  bottom: 8.0,
+);
+
 class FrameReel extends StatelessWidget {
   const FrameReel({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      scrollDirection: Axis.horizontal,
-      itemCount: 20,
-      itemBuilder: (context, index) => Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: AspectRatio(
-          aspectRatio: 16 / 9,
-          child: Container(
-            color: Colors.red,
+    return LayoutBuilder(builder: (context, constraints) {
+      final frameHeight = constraints.maxHeight - _framePadding.vertical;
+      final frameWidth = frameHeight / 9 * 16;
+      final itemWidth = frameWidth + _framePadding.horizontal;
+
+      return ListView.builder(
+        scrollDirection: Axis.horizontal,
+        padding: EdgeInsets.symmetric(
+          horizontal: (constraints.maxWidth - itemWidth) / 2,
+        ),
+        itemCount: 20,
+        itemBuilder: (context, index) => Padding(
+          padding: _framePadding,
+          child: AspectRatio(
+            aspectRatio: 16 / 9,
+            child: ColoredBox(color: Colors.red),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
