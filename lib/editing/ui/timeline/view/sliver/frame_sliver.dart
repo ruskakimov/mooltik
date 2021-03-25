412c766777578e45c7435bc1b47aae8620d853be
Rustem Kakimov <ruskakimov@gmail.com>
2021-03-25 18:41:05 +0800

    Repeat thumbnails on sliver

--- lib/editing/ui/timeline/view/sliver/frame_sliver.dart
+++ lib/editing/ui/timeline/view/sliver/frame_sliver.dart
@@ -19,17 +19,29 @@ class FrameSliver extends Sliver {
   void paint(Canvas canvas, double startY, double endY) {
     final RRect rrect = getRrect(startY, endY);
     canvas.drawRRect(rrect, Paint()..color = Colors.white);
+
     if (thumbnail != null) {
       canvas.save();
       canvas.clipRRect(rrect);
       canvas.translate(startX, startY);
       final double scaleFactor = (endY - startY) / thumbnail.height;
       canvas.scale(scaleFactor);
-      final double xOverflow = max(
-        0,
-        thumbnail.width - rrect.width / scaleFactor,
-      );
-      canvas.drawImage(thumbnail, Offset(-xOverflow / 2, 0), Paint());
+
+      final sliverWidth = rrect.width / scaleFactor;
+
+      if (thumbnail.width > sliverWidth) {
+        // Center thumbnail if it overflows sliver.
+        final xOffset = (sliverWidth - thumbnail.width) / 2;
+        canvas.drawImage(thumbnail, Offset(xOffset, 0), Paint());
+      } else {
+        // Repeat thumbnails until overflow.
+        for (double xOffset = 0;
+            xOffset < sliverWidth;
+            xOffset += thumbnail.width) {
+          canvas.drawImage(thumbnail, Offset(xOffset, 0), Paint());
+        }
+      }
+
       canvas.restore();
     }
   }
