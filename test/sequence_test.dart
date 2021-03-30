63937fb0e191d327eea4a2e3e1a2a5c87d373d46
Rustem Kakimov <ruskakimov@gmail.com>
2021-03-30 22:19:15 +0800

    Test total for multiple

--- test/sequence_test.dart
+++ test/sequence_test.dart
@@ -10,9 +10,22 @@ class TestSpan extends TimeSpan {
 
 void main() {
   group('Sequence', () {
-    test('returns correct total duration for one span', () {
+    test('handles one span', () {
       final seq = Sequence([TestSpan(Duration(milliseconds: 200))]);
       expect(seq.totalDuration, Duration(milliseconds: 200));
+    });
+
+    test('calculates correct total', () {
+      final seq = Sequence([
+        TestSpan(Duration(milliseconds: 250)),
+        TestSpan(Duration(milliseconds: 750)),
+        TestSpan(Duration(seconds: 12)),
+        TestSpan(Duration(milliseconds: 24)),
+      ]);
+      expect(
+        seq.totalDuration,
+        Duration(seconds: 13, milliseconds: 24),
+      );
     });
   });
 }
