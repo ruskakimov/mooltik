785048e9b1689dd185d054315ddcabe633903e16
Rustem Kakimov <ruskakimov@gmail.com>
2021-03-30 22:25:57 +0800

    Private list

--- lib/common/data/sequence/sequence.dart
+++ lib/common/data/sequence/sequence.dart
@@ -2,13 +2,16 @@ import 'package:mooltik/common/data/sequence/time_span.dart';
 
 /// Manages a sequence of `TimeSpan` and offers fast derived data.
 class Sequence<T extends TimeSpan> {
-  Sequence(this.spans)
-      : _totalDuration = spans.fold(
+  Sequence(List<T> spans)
+      : _spans = spans,
+        _totalDuration = spans.fold(
           Duration.zero,
           (duration, span) => duration + span.duration,
         );
 
-  final List<T> spans;
+  // TODO: Override [] operator.
+  List<T> get spans => _spans;
+  List<T> _spans;
 
   Duration get totalDuration => _totalDuration;
   Duration _totalDuration;
