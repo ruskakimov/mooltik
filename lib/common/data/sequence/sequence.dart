c714da26fe1655543daaf405b7290319e5852320
Rustem Kakimov <ruskakimov@gmail.com>
2021-04-01 11:05:52 +0800

    Dont snap to total duration on duration change

--- lib/common/data/sequence/sequence.dart
+++ lib/common/data/sequence/sequence.dart
@@ -35,9 +35,6 @@ class Sequence<T extends TimeSpan> {
     if (index < _currentIndex) {
       _currentSpanStart += diff;
       _syncIndexWithPlayhead();
-    } else if (index == _currentIndex && diff.isNegative) {
-      if (_playhead >= _totalDuration) _playhead = _totalDuration;
-      _syncIndexWithPlayhead();
     }
   }
 
