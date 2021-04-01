c43e9f318a3017c1017605cfa7cbfd9d53d300fb
Rustem Kakimov <ruskakimov@gmail.com>
2021-04-01 14:38:08 +0800

    Scene model

--- lib/common/data/project/scene_model.dart
+++ lib/common/data/project/scene_model.dart
@@ -1,5 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:mooltik/common/data/sequence/sequence.dart';
 import 'package:mooltik/common/data/sequence/time_span.dart';
+import 'package:mooltik/drawing/data/frame/frame_model.dart';
 
 class SceneModel extends TimeSpan {
-  SceneModel(Duration duration) : super(duration);
+  SceneModel({
+    @required this.frameSeq,
+    Duration duration = const Duration(seconds: 5),
+  }) : super(duration);
+
+  final Sequence<FrameModel> frameSeq;
 }
