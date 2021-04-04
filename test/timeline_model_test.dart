721e8fee0fa7cb0e6978792bf95af540c4ca5cd6
Rustem Kakimov <ruskakimov@gmail.com>
2021-04-04 17:52:16 +0800

    Fix timeline test

--- test/timeline_model_test.dart
+++ test/timeline_model_test.dart
@@ -1,44 +1,45 @@
-import 'package:flutter/painting.dart';
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mooltik/common/data/sequence/sequence.dart';
 import 'package:mooltik/drawing/data/frame/frame_model.dart';
 import 'package:mooltik/editing/data/timeline_model.dart';
-
-const Size _size = Size(1080, 720);
 
 void main() {
   group('TimelineModel', () {
     test('starts with first frame selected', () {
       final timeline = TimelineModel(
         frameSeq: Sequence<FrameModel>([
-          FrameModel(id: 1, size: _size, duration: Duration(seconds: 2)),
-          FrameModel(id: 2, size: _size, duration: Duration(seconds: 2)),
+          FrameModel(file: File('1.png'), duration: Duration(seconds: 2)),
+          FrameModel(file: File('2.png'), duration: Duration(seconds: 2)),
         ]),
         vsync: TestVSync(),
       );
-      expect(timeline.currentFrame.id, 1);
+      expect(timeline.currentFrame.file.path, '1.png');
     });
 
     test('scrubbing updates current frame', () {
       final timeline = TimelineModel(
         frameSeq: Sequence<FrameModel>([
-          FrameModel(id: 1, size: _size, duration: Duration(seconds: 2)),
-          FrameModel(id: 2, size: _size, duration: Duration(seconds: 2)),
+          FrameModel(file: File('1.png'), duration: Duration(seconds: 2)),
+          FrameModel(file: File('2.png'), duration: Duration(seconds: 2)),
         ]),
         vsync: TestVSync(),
       );
       timeline.scrub(Duration(seconds: 2));
-      expect(timeline.currentFrame.id, 2);
+      expect(timeline.currentFrame.file.path, '2.png');
       timeline.scrub(Duration(milliseconds: -1));
-      expect(timeline.currentFrame.id, 1);
+      expect(timeline.currentFrame.file.path, '1.png');
     });
 
     test('playhead position doesn\'t change when frame duration is changed',
         () {
       final timeline = TimelineModel(
         frameSeq: Sequence<FrameModel>([
-          FrameModel(id: 1, size: _size, duration: Duration(milliseconds: 500)),
-          FrameModel(id: 2, size: _size, duration: Duration(milliseconds: 500)),
+          FrameModel(
+              file: File('1.png'), duration: Duration(milliseconds: 500)),
+          FrameModel(
+              file: File('2.png'), duration: Duration(milliseconds: 500)),
         ]),
         vsync: TestVSync(),
       );
@@ -46,15 +47,17 @@ void main() {
       expect(timeline.playheadPosition, Duration(milliseconds: 250));
       timeline.changeFrameDurationAt(0, Duration(milliseconds: 900));
       expect(timeline.playheadPosition, Duration(milliseconds: 250));
-      expect(timeline.currentFrame.id, 1);
+      expect(timeline.currentFrame.file.path, '1.png');
     });
 
     test('handles frame duration change past playhead (from right to left)',
         () {
       final timeline = TimelineModel(
         frameSeq: Sequence<FrameModel>([
-          FrameModel(id: 1, size: _size, duration: Duration(milliseconds: 500)),
-          FrameModel(id: 2, size: _size, duration: Duration(milliseconds: 500)),
+          FrameModel(
+              file: File('1.png'), duration: Duration(milliseconds: 500)),
+          FrameModel(
+              file: File('2.png'), duration: Duration(milliseconds: 500)),
         ]),
         vsync: TestVSync(),
       );
@@ -62,17 +65,17 @@ void main() {
       expect(timeline.playheadPosition, Duration(milliseconds: 250));
       timeline.changeFrameDurationAt(0, Duration(milliseconds: 200));
       expect(timeline.playheadPosition, Duration(milliseconds: 250));
-      expect(timeline.currentFrame.id, 2);
+      expect(timeline.currentFrame.file.path, '2.png');
     });
 
     test('handles deleting before current frame', () {
       final timeline = TimelineModel(
         frameSeq: Sequence<FrameModel>([
-          FrameModel(id: 1, size: _size, duration: Duration(seconds: 1)),
-          FrameModel(id: 2, size: _size, duration: Duration(seconds: 2)),
-          FrameModel(id: 3, size: _size, duration: Duration(seconds: 10)),
-          FrameModel(id: 4, size: _size, duration: Duration(seconds: 4)),
-          FrameModel(id: 5, size: _size, duration: Duration(seconds: 7)),
+          FrameModel(file: File('1.png'), duration: Duration(seconds: 1)),
+          FrameModel(file: File('2.png'), duration: Duration(seconds: 2)),
+          FrameModel(file: File('3.png'), duration: Duration(seconds: 10)),
+          FrameModel(file: File('4.png'), duration: Duration(seconds: 4)),
+          FrameModel(file: File('5.png'), duration: Duration(seconds: 7)),
         ]),
         vsync: TestVSync(),
       );
@@ -80,33 +83,33 @@ void main() {
       timeline.stepForward();
       timeline.stepForward();
       expect(timeline.playheadPosition, Duration(seconds: 3));
-      expect(timeline.currentFrame.id, 3);
+      expect(timeline.currentFrame.file.path, '3.png');
       expect(timeline.currentFrameStartTime, Duration(seconds: 3));
       timeline.deleteFrameAt(1);
       expect(timeline.totalDuration, Duration(seconds: 22));
       expect(timeline.playheadPosition, Duration(seconds: 1));
-      expect(timeline.currentFrame.id, 3);
+      expect(timeline.currentFrame.file.path, '3.png');
       expect(timeline.currentFrameStartTime, Duration(seconds: 1));
     });
 
     test('handles deleting current frame', () {
       final timeline = TimelineModel(
         frameSeq: Sequence<FrameModel>([
-          FrameModel(id: 1, size: _size, duration: Duration(seconds: 20)),
-          FrameModel(id: 2, size: _size, duration: Duration(seconds: 30)),
-          FrameModel(id: 3, size: _size, duration: Duration(seconds: 7)),
-          FrameModel(id: 4, size: _size, duration: Duration(seconds: 8)),
+          FrameModel(file: File('1.png'), duration: Duration(seconds: 20)),
+          FrameModel(file: File('2.png'), duration: Duration(seconds: 30)),
+          FrameModel(file: File('3.png'), duration: Duration(seconds: 7)),
+          FrameModel(file: File('4.png'), duration: Duration(seconds: 8)),
         ]),
         vsync: TestVSync(),
       );
       timeline.stepForward();
       expect(timeline.totalDuration, Duration(seconds: 65));
-      expect(timeline.currentFrameIndex, 1);
+      expect(timeline.currentFrame.file.path, '2.png');
       expect(timeline.playheadPosition, Duration(seconds: 20));
       expect(timeline.currentFrameStartTime, Duration(seconds: 20));
       timeline.deleteFrameAt(1);
       expect(timeline.totalDuration, Duration(seconds: 35));
-      expect(timeline.currentFrame.id, 3);
+      expect(timeline.currentFrame.file.path, '3.png');
       expect(timeline.playheadPosition, Duration(seconds: 20));
       expect(timeline.currentFrameStartTime, Duration(seconds: 20));
     });
@@ -114,21 +117,21 @@ void main() {
     test('handles deleting after current frame', () {
       final timeline = TimelineModel(
         frameSeq: Sequence<FrameModel>([
-          FrameModel(id: 1, size: _size, duration: Duration(seconds: 20)),
-          FrameModel(id: 2, size: _size, duration: Duration(seconds: 30)),
-          FrameModel(id: 3, size: _size, duration: Duration(seconds: 7)),
-          FrameModel(id: 4, size: _size, duration: Duration(seconds: 8)),
+          FrameModel(file: File('1.png'), duration: Duration(seconds: 20)),
+          FrameModel(file: File('2.png'), duration: Duration(seconds: 30)),
+          FrameModel(file: File('3.png'), duration: Duration(seconds: 7)),
+          FrameModel(file: File('4.png'), duration: Duration(seconds: 8)),
         ]),
         vsync: TestVSync(),
       );
       timeline.stepForward();
       expect(timeline.totalDuration, Duration(seconds: 65));
-      expect(timeline.currentFrame.id, 2);
+      expect(timeline.currentFrame.file.path, '2.png');
       expect(timeline.playheadPosition, Duration(seconds: 20));
       expect(timeline.currentFrameStartTime, Duration(seconds: 20));
       timeline.deleteFrameAt(2);
       expect(timeline.totalDuration, Duration(seconds: 58));
-      expect(timeline.currentFrame.id, 2);
+      expect(timeline.currentFrame.file.path, '2.png');
       expect(timeline.playheadPosition, Duration(seconds: 20));
       expect(timeline.currentFrameStartTime, Duration(seconds: 20));
     });
@@ -136,23 +139,23 @@ void main() {
     test('handles deleting long frame', () {
       final timeline = TimelineModel(
         frameSeq: Sequence<FrameModel>([
-          FrameModel(id: 1, size: _size, duration: Duration(seconds: 5)),
-          FrameModel(id: 2, size: _size, duration: Duration(seconds: 300)),
-          FrameModel(id: 3, size: _size, duration: Duration(seconds: 7)),
-          FrameModel(id: 4, size: _size, duration: Duration(seconds: 8)),
-          FrameModel(id: 5, size: _size, duration: Duration(seconds: 10)),
-          FrameModel(id: 6, size: _size, duration: Duration(seconds: 10)),
+          FrameModel(file: File('1.png'), duration: Duration(seconds: 5)),
+          FrameModel(file: File('2.png'), duration: Duration(seconds: 300)),
+          FrameModel(file: File('3.png'), duration: Duration(seconds: 7)),
+          FrameModel(file: File('4.png'), duration: Duration(seconds: 8)),
+          FrameModel(file: File('5.png'), duration: Duration(seconds: 10)),
+          FrameModel(file: File('6.png'), duration: Duration(seconds: 10)),
         ]),
         vsync: TestVSync(),
       );
       timeline.scrub(Duration(seconds: 272));
       expect(timeline.totalDuration, Duration(seconds: 340));
-      expect(timeline.currentFrame.id, 2);
+      expect(timeline.currentFrame.file.path, '2.png');
       expect(timeline.playheadPosition, Duration(seconds: 272));
       expect(timeline.currentFrameStartTime, Duration(seconds: 5));
       timeline.deleteFrameAt(1);
       expect(timeline.totalDuration, Duration(seconds: 40));
-      expect(timeline.currentFrame.id, 3);
+      expect(timeline.currentFrame.file.path, '3.png');
       expect(timeline.playheadPosition, Duration(seconds: 5));
       expect(timeline.currentFrameStartTime, Duration(seconds: 5));
     });
@@ -160,19 +163,19 @@ void main() {
     test('handles deleting current last frame', () {
       final timeline = TimelineModel(
         frameSeq: Sequence<FrameModel>([
-          FrameModel(id: 1, size: _size, duration: Duration(seconds: 1)),
-          FrameModel(id: 2, size: _size, duration: Duration(seconds: 2)),
+          FrameModel(file: File('1.png'), duration: Duration(seconds: 1)),
+          FrameModel(file: File('2.png'), duration: Duration(seconds: 2)),
         ]),
         vsync: TestVSync(),
       );
       timeline.stepForward();
       expect(timeline.totalDuration, Duration(seconds: 3));
-      expect(timeline.currentFrame.id, 2);
+      expect(timeline.currentFrame.file.path, '2.png');
       expect(timeline.playheadPosition, Duration(seconds: 1));
       expect(timeline.currentFrameStartTime, Duration(seconds: 1));
       timeline.deleteFrameAt(1);
       expect(timeline.totalDuration, Duration(seconds: 1));
-      expect(timeline.currentFrame.id, 1);
+      expect(timeline.currentFrame.file.path, '1.png');
       expect(timeline.playheadPosition, Duration(seconds: 1));
       expect(timeline.currentFrameStartTime, Duration.zero);
     });
@@ -180,23 +183,23 @@ void main() {
     test('handles inserting frame after current', () {
       final timeline = TimelineModel(
         frameSeq: Sequence<FrameModel>([
-          FrameModel(id: 1, size: _size, duration: Duration(seconds: 1)),
-          FrameModel(id: 2, size: _size, duration: Duration(seconds: 2)),
-          FrameModel(id: 3, size: _size, duration: Duration(seconds: 3)),
+          FrameModel(file: File('1.png'), duration: Duration(seconds: 1)),
+          FrameModel(file: File('2.png'), duration: Duration(seconds: 2)),
+          FrameModel(file: File('3.png'), duration: Duration(seconds: 3)),
         ]),
         vsync: TestVSync(),
       );
       timeline.jumpTo(Duration(milliseconds: 1500));
       expect(timeline.totalDuration, Duration(seconds: 6));
-      expect(timeline.currentFrame.id, 2);
+      expect(timeline.currentFrame.file.path, '2.png');
       expect(timeline.playheadPosition, Duration(milliseconds: 1500));
       expect(timeline.currentFrameStartTime, Duration(seconds: 1));
       timeline.insertFrameAt(
         2,
-        FrameModel(id: 4, size: _size, duration: Duration(seconds: 4)),
+        FrameModel(file: File('4.png'), duration: Duration(seconds: 4)),
       );
       expect(timeline.totalDuration, Duration(seconds: 10));
-      expect(timeline.currentFrame.id, 2);
+      expect(timeline.currentFrame.file.path, '2.png');
       expect(timeline.playheadPosition, Duration(milliseconds: 1500));
       expect(timeline.currentFrameStartTime, Duration(seconds: 1));
     });
@@ -204,23 +207,23 @@ void main() {
     test('handles inserting frame before current', () {
       final timeline = TimelineModel(
         frameSeq: Sequence<FrameModel>([
-          FrameModel(id: 1, size: _size, duration: Duration(seconds: 1)),
-          FrameModel(id: 2, size: _size, duration: Duration(seconds: 2)),
-          FrameModel(id: 3, size: _size, duration: Duration(seconds: 2)),
+          FrameModel(file: File('1.png'), duration: Duration(seconds: 1)),
+          FrameModel(file: File('2.png'), duration: Duration(seconds: 2)),
+          FrameModel(file: File('3.png'), duration: Duration(seconds: 2)),
         ]),
         vsync: TestVSync(),
       );
       timeline.scrub(Duration(milliseconds: 2500));
       expect(timeline.totalDuration, Duration(seconds: 5));
-      expect(timeline.currentFrame.id, 2);
+      expect(timeline.currentFrame.file.path, '2.png');
       expect(timeline.playheadPosition, Duration(milliseconds: 2500));
       expect(timeline.currentFrameStartTime, Duration(seconds: 1));
       timeline.insertFrameAt(
         0,
-        FrameModel(id: 4, size: _size, duration: Duration(seconds: 3)),
+        FrameModel(file: File('4.png'), duration: Duration(seconds: 3)),
       );
       expect(timeline.totalDuration, Duration(seconds: 8));
-      expect(timeline.currentFrame.id, 2);
+      expect(timeline.currentFrame.file.path, '2.png');
       expect(timeline.playheadPosition, Duration(milliseconds: 5500));
       expect(timeline.currentFrameStartTime, Duration(seconds: 4));
     });
