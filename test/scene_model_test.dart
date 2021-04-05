3f9016e6a1089ea7d5f90afcf708b9e9841103fa
Rustem Kakimov <ruskakimov@gmail.com>
2021-04-05 19:30:37 +0800

    Test loop mode

--- test/scene_model_test.dart
+++ test/scene_model_test.dart
@@ -21,5 +21,26 @@ void main() {
       expect(scene.frameAt(Duration(seconds: 10)).file.path, '2.png');
       expect(scene.frameAt(Duration(seconds: 20)).file.path, '2.png');
     });
+
+    test('handles loop mode', () {
+      final scene = SceneModel(
+        frames: [
+          FrameModel(file: File('1.png'), duration: Duration(seconds: 2)),
+          FrameModel(file: File('2.png'), duration: Duration(seconds: 2)),
+        ],
+        duration: Duration(seconds: 20),
+        playMode: PlayMode.loop,
+      );
+      expect(scene.frameAt(Duration(seconds: 1)).file.path, '1.png');
+      expect(scene.frameAt(Duration(seconds: 2)).file.path, '2.png');
+      expect(scene.frameAt(Duration(seconds: 3)).file.path, '2.png');
+      expect(scene.frameAt(Duration(seconds: 4)).file.path, '1.png');
+      expect(scene.frameAt(Duration(seconds: 5)).file.path, '1.png');
+      expect(scene.frameAt(Duration(seconds: 6)).file.path, '2.png');
+      expect(scene.frameAt(Duration(seconds: 7)).file.path, '2.png');
+      expect(scene.frameAt(Duration(seconds: 8)).file.path, '1.png');
+      expect(scene.frameAt(Duration(seconds: 10)).file.path, '2.png');
+      expect(scene.frameAt(Duration(seconds: 20)).file.path, '1.png');
+    });
   });
 }
