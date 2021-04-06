eefcb54bed42555eec68045e599c834a54beab7a
Rustem Kakimov <ruskakimov@gmail.com>
2021-04-06 15:07:46 +0800

    Fix add scene

--- lib/editing/ui/timeline/actionbar/add_scene_button.dart
+++ lib/editing/ui/timeline/actionbar/add_scene_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mooltik/common/data/project/project.dart';
 import 'package:mooltik/common/ui/app_icon_button.dart';
 import 'package:mooltik/editing/data/timeline_model.dart';
 import 'package:provider/provider.dart';
@@ -10,7 +11,13 @@ class AddSceneButton extends StatelessWidget {
     final timeline = context.watch<TimelineModel>();
     return AppIconButton(
       icon: FontAwesomeIcons.plus,
-      onTap: timeline.isPlaying ? null : timeline.addNewSceneAfterCurrent,
+      onTap: timeline.isPlaying ? null : () => _addSceneAfterCurrent(context),
     );
+  }
+
+  Future<void> _addSceneAfterCurrent(BuildContext context) async {
+    final newScene = await context.read<Project>().createNewScene();
+    final scenes = context.read<TimelineModel>().sceneSeq;
+    scenes.insert(scenes.currentIndex + 1, newScene);
   }
 }
