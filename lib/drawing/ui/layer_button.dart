7f077ca66b54167ab18eabae7c538ee74070786a
Rustem Kakimov <ruskakimov@gmail.com>
2021-05-03 17:38:40 +0600

    Open layers sheet

--- lib/drawing/ui/layer_button.dart
+++ lib/drawing/ui/layer_button.dart
@@ -12,7 +12,22 @@ class LayerButton extends StatelessWidget {
       mini: true,
       elevation: 2,
       child: Icon(FontAwesomeIcons.layerGroup, size: 20),
-      onPressed: () {},
+      onPressed: () => _openLayersSheet(context),
+    );
+  }
+
+  void _openLayersSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => Container(height: 300),
+      shape: _roundedTopCorners,
     );
   }
 }
+
+const _roundedTopCorners = RoundedRectangleBorder(
+  borderRadius: BorderRadius.only(
+    topLeft: Radius.circular(16),
+    topRight: Radius.circular(16),
+  ),
+);
