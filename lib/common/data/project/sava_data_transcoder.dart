85937a8a822f5d8280e230f73d11060c0d24143f
Rustem Kakimov <ruskakimov@gmail.com>
2021-05-12 22:24:14 +0600

    Transcode v0.9 to v1.0

--- lib/common/data/project/sava_data_transcoder.dart
+++ lib/common/data/project/sava_data_transcoder.dart
@@ -31,9 +31,29 @@ class SaveDataTranscoder {
     };
   }
 
-  bool is_v0_9(Map<String, dynamic> json) => null;
+  bool is_v0_9(Map<String, dynamic> json) =>
+      json.containsKey('scenes') &&
+      ((json['scenes'] as List).first as Map<String, dynamic>)
+          .containsKey('frames');
 
   Map<String, dynamic> convert_v0_9_to_v1_0(Map<String, dynamic> json) {
     if (!is_v0_9(json)) throw Exception('Data is not in v0.9 format.');
+
+    return {
+      'width': json['width'],
+      'height': json['height'],
+      'scenes': (json['scenes'] as List)
+          .map((sceneData) => {
+                'layers': [
+                  {
+                    'frames': sceneData['frames'],
+                    'play_mode': sceneData['play_mode'],
+                  }
+                ],
+                'duration': sceneData['duration'],
+              })
+          .toList(),
+      'sounds': json['sounds'],
+    };
   }
 }
