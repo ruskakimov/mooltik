9a8e7cd5c2f0589cb9fd24af00d5dd4aab7f8487
Rustem Kakimov <ruskakimov@gmail.com>
2021-05-12 21:19:32 +0600

    First transcoder test

---
+++ lib/common/data/project/sava_data_transcoder.dart
@@ -0,0 +1,3 @@
+class SaveDataTranscoder {
+  Map<String, dynamic> convert_v0_8_to_v0_9(Map<String, dynamic> json) {}
+}
---
+++ test/save_data_transcoder_test.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mooltik/common/data/project/sava_data_transcoder.dart';
+
+// To solve the issue of inconsistent directory from where tests are run (https://github.com/flutter/flutter/issues/20907).
+String testData(String filePath) {
+  var dir = Directory.current.path;
+  if (dir.endsWith('/test')) {
+    dir = dir.replaceAll('/test', '');
+  }
+  return File('$dir/test/$filePath')
+      .readAsStringSync()
+      .replaceAll(RegExp(r'\s'), '');
+}
+
+void main() {
+  group('SaveDataTranscoder should', () {
+    test('transcode v0.8 to v0.9', () {
+      final transcoder = SaveDataTranscoder();
+      final data_v0_8 = testData('project_data/a_v0_8.json');
+      final data_v0_9 = testData('project_data/a_v0_8.json');
+      final transcodedJson =
+          transcoder.convert_v0_8_to_v0_9(jsonDecode(data_v0_8));
+      expect(
+        jsonEncode(transcodedJson),
+        data_v0_9,
+      );
+    });
+  });
+}
