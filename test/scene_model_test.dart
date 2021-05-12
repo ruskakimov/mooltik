574c7f7f29eb3f612e91820b596cf47f8933bf58
Rustem Kakimov <ruskakimov@gmail.com>
2021-05-12 14:01:28 +0600

    Fix remaining scene tests

--- test/scene_model_test.dart
+++ test/scene_model_test.dart
@@ -87,19 +87,15 @@ void main() {
     });
 
     test('returns correct export frames for extend last mode', () {
-      final scene = Scene(
-        layers: [
-          SceneLayer(
-            Sequence<Frame>([
-              Frame(file: File('1.png'), duration: Duration(seconds: 1)),
-              Frame(file: File('2.png'), duration: Duration(seconds: 2)),
-            ]),
-            PlayMode.extendLast,
-          ),
-        ],
-        duration: Duration(seconds: 10),
+      final sceneLayer = SceneLayer(
+        Sequence<Frame>([
+          Frame(file: File('1.png'), duration: Duration(seconds: 1)),
+          Frame(file: File('2.png'), duration: Duration(seconds: 2)),
+        ]),
+        PlayMode.extendLast,
       );
-      expect(scene.exportFrames.toList(), [
+      final duration = Duration(seconds: 10);
+      expect(sceneLayer.getExportFrames(duration).toList(), [
         Frame(file: File('1.png'), duration: Duration(seconds: 1)),
         Frame(file: File('2.png'), duration: Duration(seconds: 2)),
         Frame(file: File('2.png'), duration: Duration(seconds: 7)),
@@ -107,19 +103,15 @@ void main() {
     });
 
     test('returns correct export frames for loop mode', () {
-      final scene = Scene(
-        layers: [
-          SceneLayer(
-            Sequence<Frame>([
-              Frame(file: File('1.png'), duration: Duration(seconds: 1)),
-              Frame(file: File('2.png'), duration: Duration(seconds: 2)),
-            ]),
-            PlayMode.loop,
-          ),
-        ],
-        duration: Duration(seconds: 10),
+      final sceneLayer = SceneLayer(
+        Sequence<Frame>([
+          Frame(file: File('1.png'), duration: Duration(seconds: 1)),
+          Frame(file: File('2.png'), duration: Duration(seconds: 2)),
+        ]),
+        PlayMode.loop,
       );
-      expect(scene.exportFrames.toList(), [
+      final duration = Duration(seconds: 10);
+      expect(sceneLayer.getExportFrames(duration).toList(), [
         Frame(file: File('1.png'), duration: Duration(seconds: 1)),
         Frame(file: File('2.png'), duration: Duration(seconds: 2)),
         Frame(file: File('1.png'), duration: Duration(seconds: 1)),
@@ -131,20 +123,16 @@ void main() {
     });
 
     test('returns correct export frames for ping-pong mode', () {
-      final scene = Scene(
-        layers: [
-          SceneLayer(
-            Sequence<Frame>([
-              Frame(file: File('1.png'), duration: Duration(seconds: 1)),
-              Frame(file: File('2.png'), duration: Duration(seconds: 2)),
-              Frame(file: File('3.png'), duration: Duration(seconds: 3)),
-            ]),
-            PlayMode.pingPong,
-          ),
-        ],
-        duration: Duration(seconds: 24),
+      final sceneLayer = SceneLayer(
+        Sequence<Frame>([
+          Frame(file: File('1.png'), duration: Duration(seconds: 1)),
+          Frame(file: File('2.png'), duration: Duration(seconds: 2)),
+          Frame(file: File('3.png'), duration: Duration(seconds: 3)),
+        ]),
+        PlayMode.pingPong,
       );
-      expect(scene.exportFrames.toList(), [
+      final duration = Duration(seconds: 24);
+      expect(sceneLayer.getExportFrames(duration).toList(), [
         Frame(file: File('1.png'), duration: Duration(seconds: 1)),
         Frame(file: File('2.png'), duration: Duration(seconds: 2)),
         Frame(file: File('3.png'), duration: Duration(seconds: 3)),
