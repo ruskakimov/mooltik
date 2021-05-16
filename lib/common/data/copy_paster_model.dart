52acc9694bb3715e991faab7896629618a5d3db9
Rustem Kakimov <ruskakimov@gmail.com>
2021-05-16 21:08:57 +0600

    Copy paster (#146)
    * Add copy cut paste row
    
    * Paint splash on top of separator
    
    * Copy paster
    
    * Push new snapshot on paste
    
    * Implement paste
    
    * Bump version

---
+++ lib/common/data/copy_paster_model.dart
@@ -0,0 +1,46 @@
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:mooltik/common/data/io/generate_image.dart';
+
+class CopyPasterModel extends ChangeNotifier {
+  ui.Image get copiedImage => _copiedImage;
+  ui.Image _copiedImage;
+
+  void copyImage(ui.Image image) {
+    _copiedImage = image;
+    notifyListeners();
+  }
+
+  bool get canPaste => _copiedImage != null;
+
+  Future<ui.Image> pasteOn(ui.Image destination) async {
+    return generateImage(
+      PastePainter(source: _copiedImage, destination: destination),
+      destination.width,
+      destination.height,
+    );
+  }
+}
+
+class PastePainter extends CustomPainter {
+  PastePainter({
+    @required this.source,
+    @required this.destination,
+  });
+
+  final ui.Image source;
+  final ui.Image destination;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawImage(destination, Offset.zero, Paint());
+    canvas.drawImage(source, Offset.zero, Paint());
+  }
+
+  @override
+  bool shouldRepaint(PastePainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(PastePainter oldDelegate) => false;
+}
--- lib/drawing/ui/reel/frame_menu.dart
+++ lib/drawing/ui/reel/frame_menu.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mooltik/common/data/copy_paster_model.dart';
 import 'package:mooltik/common/ui/labeled_icon_button.dart';
+import 'package:mooltik/drawing/data/easel_model.dart';
 import 'package:mooltik/drawing/data/frame_reel_model.dart';
 import 'package:provider/provider.dart';
 
@@ -21,68 +23,125 @@ class FrameMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final reel = context.watch<FrameReelModel>();
+    final copyPaster = context.watch<CopyPasterModel>();
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Row(
+      child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          LabeledIconButton(
-            icon: FontAwesomeIcons.plusSquare,
-            label: 'Add before',
-            color: Theme.of(context).colorScheme.onPrimary,
-            onTap: () async {
-              await reel.addBeforeCurrent();
-              jumpTo(reel.currentIndex); // Keep current centered.
-              closePopup();
-
-              await Future.delayed(scrollDelay);
-
-              // Scroll to new frame.
-              scrollTo(reel.currentIndex - 1);
-            },
-          ),
-          LabeledIconButton(
-            icon: FontAwesomeIcons.copy,
-            label: 'Duplicate',
-            color: Theme.of(context).colorScheme.onPrimary,
-            onTap: () async {
-              await reel.duplicateCurrent();
-              closePopup();
-
-              await Future.delayed(scrollDelay);
-
-              // Scroll to duplicated frame.
-              scrollTo(reel.currentIndex + 1);
-            },
-          ),
-          LabeledIconButton(
-            icon: FontAwesomeIcons.trashAlt,
-            label: 'Delete',
-            color: Theme.of(context).colorScheme.onPrimary,
-            onTap: reel.canDeleteCurrent
-                ? () {
-                    reel.deleteCurrent();
-                    closePopup();
-                  }
-                : null,
-          ),
-          LabeledIconButton(
-            icon: FontAwesomeIcons.plusSquare,
-            label: 'Add after',
-            color: Theme.of(context).colorScheme.onPrimary,
-            onTap: () async {
-              await reel.addAfterCurrent();
-              closePopup();
-
-              await Future.delayed(scrollDelay);
-
-              // Scroll to new frame.
-              scrollTo(reel.currentIndex + 1);
-            },
-          ),
+          _buildFirstRow(context, reel, copyPaster),
+          _buildSeparator(),
+          _buildSecondRow(context, reel),
         ],
       ),
+    );
+  }
+
+  Widget _buildSeparator() {
+    return Ink(
+      width: 150,
+      height: 1,
+      color: Colors.black12,
+    );
+  }
+
+  Row _buildFirstRow(
+    BuildContext context,
+    FrameReelModel reel,
+    CopyPasterModel copyPaster,
+  ) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        LabeledIconButton(
+          icon: Icons.copy_rounded,
+          label: 'Copy',
+          color: Theme.of(context).colorScheme.onPrimary,
+          onTap: () {
+            copyPaster.copyImage(reel.currentFrame.snapshot);
+            closePopup();
+          },
+        ),
+        LabeledIconButton(
+          icon: Icons.cut_rounded,
+          label: 'Cut',
+          color: Theme.of(context).colorScheme.onPrimary,
+          onTap: reel.canDeleteCurrent
+              ? () {
+                  copyPaster.copyImage(reel.currentFrame.snapshot);
+                  reel.deleteCurrent();
+                  closePopup();
+                }
+              : null,
+        ),
+        LabeledIconButton(
+          icon: Icons.paste_rounded,
+          label: 'Paste',
+          color: Theme.of(context).colorScheme.onPrimary,
+          onTap: copyPaster.canPaste
+              ? () async {
+                  closePopup();
+                  final easel = context.read<EaselModel>();
+                  final newSnapshot = await copyPaster.pasteOn(
+                    easel.frame.snapshot,
+                  );
+                  easel.pushSnapshot(newSnapshot);
+                }
+              : null,
+        ),
+      ],
+    );
+  }
+
+  Row _buildSecondRow(BuildContext context, FrameReelModel reel) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        LabeledIconButton(
+          icon: FontAwesomeIcons.plusSquare,
+          label: 'Add before',
+          color: Theme.of(context).colorScheme.onPrimary,
+          onTap: () async {
+            await reel.addBeforeCurrent();
+            jumpTo(reel.currentIndex); // Keep current centered.
+            closePopup();
+
+            await Future.delayed(scrollDelay);
+
+            // Scroll to new frame.
+            scrollTo(reel.currentIndex - 1);
+          },
+        ),
+        LabeledIconButton(
+          icon: FontAwesomeIcons.copy,
+          label: 'Duplicate',
+          color: Theme.of(context).colorScheme.onPrimary,
+          onTap: () async {
+            await reel.duplicateCurrent();
+            closePopup();
+
+            await Future.delayed(scrollDelay);
+
+            // Scroll to duplicated frame.
+            scrollTo(reel.currentIndex + 1);
+          },
+        ),
+        LabeledIconButton(
+          icon: FontAwesomeIcons.plusSquare,
+          label: 'Add after',
+          color: Theme.of(context).colorScheme.onPrimary,
+          onTap: () async {
+            await reel.addAfterCurrent();
+            closePopup();
+
+            await Future.delayed(scrollDelay);
+
+            // Scroll to new frame.
+            scrollTo(reel.currentIndex + 1);
+          },
+        ),
+      ],
     );
   }
 }
