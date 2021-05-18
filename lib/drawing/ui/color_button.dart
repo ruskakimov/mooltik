0201b8e0b15f296c7a5c1998947d907146df077e
Rustem Kakimov <ruskakimov@gmail.com>
2021-05-18 12:52:42 +0600

    Style color picker button

--- lib/drawing/ui/color_button.dart
+++ lib/drawing/ui/color_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mooltik/common/ui/popup_with_arrow.dart';
 import 'package:mooltik/drawing/data/toolbox/tools/tools.dart';
 import 'package:mooltik/drawing/ui/color_picker.dart';
+import 'package:mooltik/drawing/ui/picker_option_button.dart';
 import 'package:provider/provider.dart';
 import 'package:mooltik/drawing/data/toolbox/toolbox_model.dart';
 
@@ -20,47 +21,49 @@ class _ColorButtonState extends State<ColorButton> {
     final toolbox = context.watch<ToolboxModel>();
 
     return PopupWithArrowEntry(
-        visible: _pickerOpen,
-        arrowSide: ArrowSide.top,
-        arrowAnchor: const Alignment(0, 0.6),
-        arrowSidePosition: ArrowSidePosition.middle,
-        popupBody: SizedBox(
-          width: 180,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ColorPicker(
-                selectedColor: toolbox.selectedTool.color,
-                colorOptions: const [
-                  Colors.black,
-                  Colors.redAccent,
-                  Colors.yellow,
-                  Colors.teal,
-                  Colors.blue,
-                  Colors.deepPurple,
-                ],
-                onSelected: (Color color) {
-                  toolbox.changeToolColor(color);
-                  _closePicker();
-                },
-              ),
-            ],
-          ),
+      visible: _pickerOpen,
+      arrowSide: ArrowSide.top,
+      arrowAnchor: const Alignment(0, 0.6),
+      arrowSidePosition: ArrowSidePosition.middle,
+      popupBody: SizedBox(
+        width: 180,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ColorPicker(
+              selectedColor: toolbox.selectedTool.color,
+              colorOptions: const [
+                Colors.black,
+                Colors.redAccent,
+                Colors.yellow,
+                Colors.teal,
+                Colors.blue,
+                Colors.deepPurple,
+              ],
+              onSelected: (Color color) {
+                toolbox.changeToolColor(color);
+                _closePicker();
+              },
+            ),
+          ],
         ),
-        onTapOutside: _closePicker,
-        onDragOutside: _closePicker,
-        child: GestureDetector(
+      ),
+      onTapOutside: _closePicker,
+      onDragOutside: _closePicker,
+      child: SizedBox(
+        width: 52,
+        child: PickerOptionButton(
+          selected: true,
+          size: 32,
+          innerCircleColor: toolbox.selectedTool.color,
           onTap: () {
             if (toolbox.selectedTool is! Eraser) {
               _openPicker();
             }
           },
-          child: Container(
-            width: 52,
-            height: 44,
-            color: toolbox.selectedTool.color,
-          ),
-        ));
+        ),
+      ),
+    );
   }
 
   void _openPicker() {
