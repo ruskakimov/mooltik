4619ea17cedf4c12c9c7530abd2cad99176e3859
Rustem Kakimov <ruskakimov@gmail.com>
2021-05-18 12:24:44 +0600

    Center color button

--- lib/drawing/ui/toolbar.dart
+++ lib/drawing/ui/toolbar.dart
@@ -11,15 +11,24 @@ class Toolbar extends StatelessWidget {
   Widget build(BuildContext context) {
     final toolbox = context.watch<ToolboxModel>();
 
+    final toolCount = toolbox.tools.length;
+    final leftTools = toolbox.tools.sublist(0, toolCount ~/ 2);
+    final rightTools = toolbox.tools.sublist(toolCount ~/ 2);
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        for (final tool in toolbox.tools)
+        for (final tool in leftTools)
           ToolButton(
             tool: tool,
             selected: tool == toolbox.selectedTool,
           ),
         ColorButton(),
+        for (final tool in rightTools)
+          ToolButton(
+            tool: tool,
+            selected: tool == toolbox.selectedTool,
+          ),
       ],
     );
   }
