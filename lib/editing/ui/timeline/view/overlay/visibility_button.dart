c95edce0cad0ca4bb9b5709b0424ad47adbff2ad
Rustem Kakimov <ruskakimov@gmail.com>
2021-05-24 00:19:56 +0600

    Layer visibility button on timeline

--- lib/editing/ui/timeline/view/overlay/visibility_button.dart
+++ lib/editing/ui/timeline/view/overlay/visibility_button.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:mooltik/common/data/project/scene_layer.dart';
 import 'package:mooltik/editing/data/timeline_view_model.dart';
 import 'package:mooltik/editing/ui/timeline/view/overlay/timeline_positioned.dart';
 import 'package:provider/provider.dart';
 
-class PlayModeButton extends StatelessWidget {
-  const PlayModeButton({
+class VisibilityButton extends StatelessWidget {
+  const VisibilityButton({
     Key key,
     @required this.layerIndex,
   }) : super(key: key);
@@ -15,33 +14,21 @@ class PlayModeButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final timelineView = context.watch<TimelineViewModel>();
-    final playMode = timelineView.sceneLayers[layerIndex].playMode;
+    final visible = timelineView.sceneLayers[layerIndex].visible;
 
     return TimelinePositioned(
       timestamp: timelineView.sceneStart,
       y: timelineView.rowMiddle(layerIndex),
       width: 48,
       height: 48,
-      offset: Offset(-32, 0),
+      offset: Offset(-80, 0),
       child: IconButton(
         icon: Icon(
-          _getIcon(playMode),
+          visible ? Icons.visibility_outlined : Icons.visibility_off_outlined,
           color: Theme.of(context).colorScheme.onBackground,
         ),
         onPressed: () => timelineView.nextScenePlayModeForLayer(layerIndex),
       ),
     );
-  }
-
-  IconData _getIcon(PlayMode playMode) {
-    switch (playMode) {
-      case PlayMode.extendLast:
-        return Icons.trending_flat_rounded;
-      case PlayMode.loop:
-        return Icons.autorenew_rounded;
-      case PlayMode.pingPong:
-        return Icons.sync_alt_rounded;
-    }
-    return null;
   }
 }
