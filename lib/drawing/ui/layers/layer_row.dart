aedc16df1118a61251ca14c48d32cf722a598050
Rustem Kakimov <ruskakimov@gmail.com>
2021-06-10 15:31:46 +0600

    Refactor

--- lib/drawing/ui/layers/layer_row.dart
+++ lib/drawing/ui/layers/layer_row.dart
@@ -24,42 +24,60 @@ class LayerRow extends StatelessWidget {
     final selected = reel == reelStack.activeReel;
     final visible = reelStack.isVisible(reelStack.reels.indexOf(reel));
 
-    return Slidable(
-      actionPane: SlidableDrawerActionPane(),
-      closeOnScroll: true,
-      secondaryActions: [DeleteLayerSlideAction(reelToDelete: reel)],
-      child: SizedBox(
-        height: 80,
-        child: Material(
-          color: selected ? Colors.white24 : Colors.transparent,
-          child: InkWell(
-            onTap: () => reelStack.changeActiveReel(reel),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                _buildCell(),
-                SizedBox(width: 4),
-                _buildLabel(context),
-                Spacer(),
-                VisibilitySwitch(
-                  value: visible,
-                  onChanged: (value) {
-                    final reelStack = context.read<ReelStackModel>();
-                    reelStack.setLayerVisibility(
-                      reelStack.reels.indexOf(reel),
-                      value,
-                    );
-                  },
-                ),
-              ],
-            ),
+    final content = SizedBox(
+      height: 80,
+      child: Material(
+        color: selected ? Colors.white24 : Colors.transparent,
+        child: InkWell(
+          onTap: () => reelStack.changeActiveReel(reel),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Preview(reel: reel),
+              SizedBox(width: 4),
+              _buildLabel(context),
+              Spacer(),
+              VisibilitySwitch(
+                value: visible,
+                onChanged: (value) {
+                  final reelStack = context.read<ReelStackModel>();
+                  reelStack.setLayerVisibility(
+                    reelStack.reels.indexOf(reel),
+                    value,
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
     );
+
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      closeOnScroll: true,
+      secondaryActions: [DeleteLayerSlideAction(reelToDelete: reel)],
+      child: content,
+    );
   }
 
-  Widget _buildCell() {
+  Widget _buildLabel(BuildContext context) {
+    final count = reel.frameSeq.length;
+    final appendix = count > 1 ? 'frames' : 'frame';
+    return Text('$count $appendix');
+  }
+}
+
+class Preview extends StatelessWidget {
+  const Preview({
+    Key? key,
+    required this.reel,
+  }) : super(key: key);
+
+  final FrameReelModel reel;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(8.0),
       clipBehavior: Clip.antiAlias,
@@ -80,12 +98,6 @@ class LayerRow extends StatelessWidget {
         ],
       ),
     );
-  }
-
-  Widget _buildLabel(BuildContext context) {
-    final count = reel.frameSeq.length;
-    final appendix = count > 1 ? 'frames' : 'frame';
-    return Text('$count $appendix');
   }
 }
 
