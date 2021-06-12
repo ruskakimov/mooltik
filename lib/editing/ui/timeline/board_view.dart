96b39441bbe045ebee891f1ddeb810415c4f5bbe
Rustem Kakimov <ruskakimov@gmail.com>
2021-06-12 14:28:34 +0600

    Board view stub

--- lib/editing/ui/timeline/board_view.dart
+++ lib/editing/ui/timeline/board_view.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:mooltik/common/data/project/scene.dart';
+import 'package:mooltik/editing/data/timeline_model.dart';
+import 'package:provider/provider.dart';
 
 class BoardView extends StatelessWidget {
   const BoardView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final timeline = context.watch<TimelineModel>();
+
+    final scenes = timeline.sceneSeq.iterable;
+
+    return ReorderableListView(
+      scrollDirection: Axis.horizontal,
+      onReorder: (i, j) {},
+      children: [
+        for (final scene in scenes)
+          Board(
+            key: Key(scene.allFrames.first.file.path),
+            scene: scene,
+          ),
+      ],
+    );
+  }
+}
+
+class Board extends StatelessWidget {
+  const Board({
+    Key? key,
+    required this.scene,
+  }) : super(key: key);
+
+  final Scene scene;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 160,
+      color: Colors.red,
+    );
   }
 }
