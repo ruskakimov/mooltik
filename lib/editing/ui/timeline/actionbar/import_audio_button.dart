f6d656caf02d335e0eefd954e53d1c786d82de56
Rustem Kakimov <ruskakimov@gmail.com>
2021-06-21 18:39:34 +0300

    Handle perm denied storage on audio import

--- lib/editing/ui/timeline/actionbar/import_audio_button.dart
+++ lib/editing/ui/timeline/actionbar/import_audio_button.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:mooltik/common/data/project/project.dart';
+import 'package:mooltik/common/ui/open_allow_access_dialog.dart';
 import 'package:mooltik/editing/data/timeline_model.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mooltik/common/ui/app_icon_button.dart';
@@ -37,24 +39,35 @@ class ImportAudioButton extends StatelessWidget {
 
         return AppIconButton(
           icon: FontAwesomeIcons.music,
-          onTap: playing
-              ? null
-              : () async {
-                  // TODO: Handle permanently denied storage permission
-                  try {
-                    final project = context.read<Project>();
-                    await importer.importAudioTo(project);
-                  } catch (e, stack) {
-                    _showFailedImportError(context);
-                    FirebaseCrashlytics.instance.recordError(e, stack);
-                  }
-                },
+          onTap: playing ? null : () => _handleImportAudioTap(context),
         );
       },
     );
   }
 
-  void _showFailedImportError(BuildContext context) {
+  Future<void> _handleImportAudioTap(BuildContext context) async {
+    final storageStatus = await Permission.storage.status;
+
+    if (storageStatus.isPermanentlyDenied) {
+      openAllowAccessDialog(context: context, name: 'Storage');
+    } else {
+      _importAudio(context);
+    }
+  }
+
+  Future<void> _importAudio(BuildContext context) async {
+    final importer = context.read<ImporterModel>();
+    final project = context.read<Project>();
+
+    try {
+      await importer.importAudioTo(project);
+    } catch (e, stack) {
+      _showFailedAudioImportSnack(context);
+      FirebaseCrashlytics.instance.recordError(e, stack);
+    }
+  }
+
+  void _showFailedAudioImportSnack(BuildContext context) {
     final snack = SnackBar(
       content: Text(
         'Failed to load audio.',
