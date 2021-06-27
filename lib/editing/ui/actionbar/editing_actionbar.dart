3fd29cd3cd75dfda8c146746162ea578e97ea7ec
Rustem Kakimov <ruskakimov@gmail.com>
2021-06-28 02:24:05 +0600

    New export popup

--- lib/editing/ui/actionbar/editing_actionbar.dart
+++ lib/editing/ui/actionbar/editing_actionbar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mooltik/common/ui/get_permission.dart';
 import 'package:mooltik/editing/data/exporter_model.dart';
@@ -64,6 +65,66 @@ class EditingActionbar extends StatelessWidget {
         ),
         child: ExportDialog(),
       ),
+    );
+  }
+}
+
+enum ExportOption {
+  video,
+  images,
+}
+
+class ExportDialog extends StatefulWidget {
+  const ExportDialog({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _ExportDialogState createState() => _ExportDialogState();
+}
+
+class _ExportDialogState extends State<ExportDialog> {
+  ExportOption _selectedOption = ExportOption.video;
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      title: Text('Export as'),
+      contentPadding: const EdgeInsets.all(16),
+      children: [
+        CupertinoSlidingSegmentedControl<ExportOption>(
+          backgroundColor: Colors.black.withOpacity(0.25),
+          // backgroundColor: Theme.of(context).colorScheme.secondary,
+          // thumbColor: Theme.of(context).colorScheme.primary,
+          // padding: const EdgeInsets.all(4),
+          groupValue: _selectedOption,
+          children: {
+            ExportOption.video: Text('Video'),
+            ExportOption.images: Text('Images'),
+          },
+          onValueChanged: (option) {
+            if (option == null) return;
+            setState(() {
+              _selectedOption = option;
+            });
+          },
+        ),
+        SizedBox(height: 8),
+        TextField(
+          decoration: InputDecoration(
+            labelText: 'File name',
+          ),
+          controller: TextEditingController.fromValue(
+            TextEditingValue(text: '123123.mp4'),
+          ),
+          maxLines: 1,
+        ),
+        SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Export'),
+        ),
+      ],
     );
   }
 }
