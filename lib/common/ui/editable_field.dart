537c07f6bd5ccb64058a8267f91f87cc64b3ed3f
Rustem Kakimov <ruskakimov@gmail.com>
2021-06-28 18:52:47 +0600

    Extract editable field into a file

---
+++ lib/common/ui/editable_field.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class EditableField extends StatelessWidget {
+  const EditableField({
+    Key? key,
+    required this.label,
+    required this.text,
+    this.onTap,
+  }) : super(key: key);
+
+  final String label;
+  final String text;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: InkWell(
+        onTap: onTap,
+        splashColor: Colors.transparent,
+        borderRadius: BorderRadius.circular(8),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: _buildContent(context),
+              ),
+              Icon(
+                Icons.edit,
+                size: 20,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              // SizedBox(width: 8),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContent(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 12,
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+        SizedBox(height: 4),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 16,
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/editing/ui/actionbar/editing_actionbar.dart
+++ lib/editing/ui/actionbar/editing_actionbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mooltik/common/ui/editable_field.dart';
 import 'package:mooltik/common/ui/get_permission.dart';
 import 'package:mooltik/editing/data/exporter_model.dart';
 import 'package:mooltik/editing/data/timeline_model.dart';
@@ -126,71 +127,6 @@ class _ExportDialogState extends State<ExportDialog> {
           _selectedOption = option;
         });
       },
-    );
-  }
-}
-
-class EditableField extends StatelessWidget {
-  const EditableField({
-    Key? key,
-    required this.label,
-    required this.text,
-    this.onTap,
-  }) : super(key: key);
-
-  final String label;
-  final String text;
-  final VoidCallback? onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      child: InkWell(
-        onTap: onTap,
-        splashColor: Colors.transparent,
-        borderRadius: BorderRadius.circular(8),
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: _buildContent(context),
-              ),
-              Icon(
-                Icons.edit,
-                size: 20,
-                color: Theme.of(context).colorScheme.secondary,
-              ),
-              // SizedBox(width: 8),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildContent(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text(
-          label,
-          style: TextStyle(
-            fontSize: 12,
-            color: Theme.of(context).colorScheme.secondary,
-          ),
-        ),
-        SizedBox(height: 4),
-        Text(
-          text,
-          style: TextStyle(
-            fontSize: 16,
-            color: Theme.of(context).colorScheme.onSurface,
-          ),
-        ),
-      ],
     );
   }
 }
