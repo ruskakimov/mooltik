eb42dc9b42a6852967b08f0305ee314077305f7a
Rustem Kakimov <ruskakimov@gmail.com>
2021-06-30 01:07:19 +0600

    Extract exporter form

--- lib/editing/ui/actionbar/export_dialog.dart
+++ lib/editing/ui/actionbar/export_dialog.dart
@@ -28,51 +28,23 @@ class _ExportDialogState extends State<ExportDialog> {
     return WillPopScope(
       onWillPop: () async => exporter.isInitial,
       child: SimpleDialog(
-        title: Text('Export as'),
         contentPadding: const EdgeInsets.all(16),
         children: [
-          _buildOptionMenu(),
-          _buildForm(),
+          ExporterForm(
+            selectedOption: _selectedOption,
+            onValueChanged: _onExportOptionChanged,
+          ),
           _buildButtons(),
         ],
       ),
     );
   }
 
-  Widget _buildOptionMenu() {
-    return CupertinoSlidingSegmentedControl<ExportOption>(
-      backgroundColor: Colors.black.withOpacity(0.25),
-      groupValue: _selectedOption,
-      children: {
-        ExportOption.video: Text('Video'),
-        ExportOption.images: Text('Images'),
-      },
-      onValueChanged: (option) {
-        if (option == null) return;
-        setState(() {
-          _selectedOption = option;
-        });
-      },
-    );
-  }
-
-  Widget _buildForm() {
-    return AnimatedCrossFade(
-      duration: Duration(milliseconds: 300),
-      crossFadeState: _selectedOption == ExportOption.video
-          ? CrossFadeState.showFirst
-          : CrossFadeState.showSecond,
-      firstChild: EditableField(
-        label: 'File name',
-        text: '123123.mp4',
-        onTap: () {},
-      ),
-      secondChild: EditableField(
-        label: 'Selected frames',
-        text: '148',
-        onTap: () {},
-      ),
-    );
+  void _onExportOptionChanged(option) {
+    if (option == null) return;
+    setState(() {
+      _selectedOption = option;
+    });
   }
 
   Widget _buildButtons() {
@@ -135,6 +107,74 @@ class _ExportDialogState extends State<ExportDialog> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class ExporterForm extends StatelessWidget {
+  const ExporterForm({
+    Key? key,
+    required this.selectedOption,
+    required this.onValueChanged,
+  }) : super(key: key);
+
+  final ExportOption selectedOption;
+  final ValueChanged<ExportOption?> onValueChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        _buildTitle(),
+        SizedBox(height: 8),
+        _buildOptionMenu(),
+        _buildForm(),
+      ],
+    );
+  }
+
+  Widget _buildTitle() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        'Export as',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOptionMenu() {
+    return CupertinoSlidingSegmentedControl<ExportOption>(
+      backgroundColor: Colors.black.withOpacity(0.25),
+      groupValue: selectedOption,
+      children: {
+        ExportOption.video: Text('Video'),
+        ExportOption.images: Text('Images'),
+      },
+      onValueChanged: onValueChanged,
+    );
+  }
+
+  Widget _buildForm() {
+    return AnimatedCrossFade(
+      duration: Duration(milliseconds: 300),
+      crossFadeState: selectedOption == ExportOption.video
+          ? CrossFadeState.showFirst
+          : CrossFadeState.showSecond,
+      firstChild: EditableField(
+        label: 'File name',
+        text: '123123.mp4',
+        onTap: () {},
+      ),
+      secondChild: EditableField(
+        label: 'Selected frames',
+        text: '148',
+        onTap: () {},
+      ),
     );
   }
 }
