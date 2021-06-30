34556cf47572992705471c56add32855253c0f52
Rustem Kakimov <ruskakimov@gmail.com>
2021-06-30 21:48:38 +0600

    Refactor

--- lib/editing/ui/export/exporter_preview.dart
+++ lib/editing/ui/export/exporter_preview.dart
@@ -12,29 +12,59 @@ class ExporterPreview extends StatelessWidget {
 
     return GestureDetector(
       onTap: exporter.isDone ? exporter.openOutputFile : null,
-      child: Container(
-        height: 150,
-        width: double.infinity,
-        margin: const EdgeInsets.only(bottom: 8),
-        decoration: BoxDecoration(
-          color: Colors.white10,
-          borderRadius: BorderRadius.circular(8),
-        ),
+      child: _PreviewBox(
         child: Stack(
           alignment: Alignment.center,
           children: [
             PieProgressIndicator(progress: exporter.progress),
-            AnimatedOpacity(
-              opacity: exporter.isDone ? 1 : 0,
-              duration: Duration(milliseconds: 300),
-              child: Icon(
-                Icons.play_arrow_rounded,
-                color: Colors.black,
-                size: 48,
-              ),
-            ),
+            _PlayIcon(visible: exporter.isDone),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _PreviewBox extends StatelessWidget {
+  const _PreviewBox({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      width: double.infinity,
+      margin: const EdgeInsets.only(bottom: 8),
+      decoration: BoxDecoration(
+        color: Colors.white10,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: child,
+    );
+  }
+}
+
+class _PlayIcon extends StatelessWidget {
+  const _PlayIcon({
+    Key? key,
+    required this.visible,
+  }) : super(key: key);
+
+  final bool visible;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedOpacity(
+      opacity: visible ? 1 : 0,
+      duration: Duration(milliseconds: 300),
+      child: Icon(
+        Icons.play_arrow_rounded,
+        color: Colors.black,
+        size: 48,
       ),
     );
   }
