de918db6e79b4863199171d61a9ca4d5bba601ad
Rustem Kakimov <ruskakimov@gmail.com>
2021-07-02 00:21:44 +0600

    Slide serialization methods

--- lib/common/data/io/mp4/slide.dart
+++ lib/common/data/io/mp4/slide.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:mooltik/common/data/duration_methods.dart';
+
 /// Data class for video export.
 class Slide {
   Slide(this.pngImage, this.duration);
@@ -9,4 +11,17 @@ class Slide {
 
   /// Slide duration.
   final Duration duration;
+
+  factory Slide.fromJson(Map<String, dynamic> json) => Slide(
+        File(json[_filePathKey]),
+        (json[_durationKey] as String).parseDuration(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        _filePathKey: pngImage.path,
+        _durationKey: duration.toString(),
+      };
+
+  static const String _filePathKey = 'path';
+  static const String _durationKey = 'duration';
 }
