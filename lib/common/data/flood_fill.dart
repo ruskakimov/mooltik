e414b9870a31a6c6cbd74c167213d6c442f07967
Rustem Kakimov <ruskakimov@gmail.com>
2021-07-17 22:22:12 +0600

    Flood function stub

---
+++ lib/common/data/flood_fill.dart
@@ -0,0 +1,16 @@
+import 'dart:typed_data';
+
+/// Fills image represented by [imageBytes], [imageWidth], and [imageHeight]
+/// starting at [startX], [startY]
+/// with the given [color].
+ByteData floodFill(
+  ByteData imageBytes,
+  int imageWidth,
+  int imageHeight,
+  int startX,
+  int startY,
+  int color,
+) {
+  // TODO: Implement flood fill.
+  return imageBytes;
+}
