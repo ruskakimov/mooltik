4af0f21790d1eaeb1177a6003a766a4bb8a50cf8
Rustem Kakimov <ruskakimov@gmail.com>
2021-07-18 16:50:37 +0600

    Pass rect fill test

--- lib/common/data/flood_fill.dart
+++ lib/common/data/flood_fill.dart
@@ -11,10 +11,21 @@ ByteData floodFill(
   int startY,
   int color,
 ) {
+  int getPixel(int x, int y) {
+    return imageBytes.getUint32((y * imageWidth + x) * 4);
+  }
+
+  void setPixel(int x, int y, int color) {
+    return imageBytes.setUint32((y * imageWidth + x) * 4, color);
+  }
+
+  final originalColor = getPixel(startX, startY);
+
   for (int x = 0; x < imageWidth; x++) {
     for (int y = 0; y < imageHeight; y++) {
-      final byteOffset = (y * imageWidth + x) * 4;
-      imageBytes.setUint32(byteOffset, color);
+      if (getPixel(x, y) == originalColor) {
+        setPixel(x, y, color);
+      }
     }
   }
   return imageBytes;
