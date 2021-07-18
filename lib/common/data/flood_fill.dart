b4a9127da8205d192ac8feee35c036eb8c384ef3
Rustem Kakimov <ruskakimov@gmail.com>
2021-07-18 21:01:13 +0600

    Helper class

--- lib/common/data/flood_fill.dart
+++ lib/common/data/flood_fill.dart
@@ -11,22 +11,38 @@ ByteData floodFill(
   int startY,
   int color,
 ) {
-  int getPixel(int x, int y) {
-    return imageBytes.getUint32((y * imageWidth + x) * 4);
-  }
+  final image = _Image(imageBytes, imageWidth, imageHeight);
 
-  void setPixel(int x, int y, int color) {
-    return imageBytes.setUint32((y * imageWidth + x) * 4, color);
-  }
-
-  final originalColor = getPixel(startX, startY);
+  final originalColor = image.getPixel(startX, startY);
 
   for (int x = 0; x < imageWidth; x++) {
     for (int y = 0; y < imageHeight; y++) {
-      if (getPixel(x, y) == originalColor) {
-        setPixel(x, y, color);
+      if (image.getPixel(x, y) == originalColor) {
+        image.setPixel(x, y, color);
       }
     }
   }
   return imageBytes;
+}
+
+class _Image {
+  _Image(this.bytes, this.width, this.height);
+
+  final ByteData bytes;
+  final int width;
+  final int height;
+
+  bool withinBounds(int x, int y) {
+    return x >= 0 && x < width && y >= 0 && y < height;
+  }
+
+  int _byteOffset(int x, int y) => (y * width + x) * 4;
+
+  int getPixel(int x, int y) {
+    return bytes.getUint32(_byteOffset(x, y));
+  }
+
+  void setPixel(int x, int y, int color) {
+    return bytes.setUint32(_byteOffset(x, y), color);
+  }
 }
