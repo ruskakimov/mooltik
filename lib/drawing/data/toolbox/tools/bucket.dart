2ffff65ad8fa3e225db570b95a5c9769df18de07
Rustem Kakimov <ruskakimov@gmail.com>
2021-07-23 16:51:54 +0600

    Plug in flood algo

--- lib/drawing/data/toolbox/tools/bucket.dart
+++ lib/drawing/data/toolbox/tools/bucket.dart
@@ -2,6 +2,8 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:mooltik/common/data/flood_fill.dart';
+import 'package:mooltik/common/data/io/image.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'tool.dart';
 
@@ -29,8 +31,22 @@ class Bucket extends ToolWithColor {
   void onStrokeCancel() {}
 
   /// Returns [source] image with fill starting at [startX] and [startY].
-  ui.Image applyBucketAt(ui.Image source, int startX, int startY) {
-    color; // selected color
-    return source;
+  Future<ui.Image> applyBucketAt(
+    ui.Image source,
+    int startX,
+    int startY,
+  ) async {
+    final imageByteData = await source.toByteData();
+
+    final resultByteData = floodFill(
+      imageByteData!,
+      source.width,
+      source.height,
+      startX,
+      startY,
+      color.value,
+    );
+
+    return imageFromBytes(resultByteData, source.width, source.height);
   }
 }
