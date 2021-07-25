5e047be972505b8604a486b0c9b36bffdc33b728
Rustem Kakimov <ruskakimov@gmail.com>
2021-07-25 15:02:13 +0600

    Fix test

--- test/task_queue_test.dart
+++ test/task_queue_test.dart
@@ -1,15 +1,28 @@
+import 'package:fake_async/fake_async.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mooltik/common/data/task_queue.dart';
 
 void main() {
   group('TaskQueue', () {
     test('runs a single task immediately', () {
-      final q = TaskQueue();
-      final s = Stopwatch();
-      q.add(() async {
-        s.stop();
-        expect(s.elapsed, Duration.zero);
+      fakeAsync((async) {
+        final q = TaskQueue();
+        var done = false;
+
+        q.add(() async {
+          await Future.delayed(Duration(seconds: 5));
+          done = true;
+        });
+
+        async.elapse(Duration(seconds: 5));
+        expect(done, isTrue);
       });
+    });
+
+    test('runs two tasks consecutively', () {
+      final q = TaskQueue();
+      q.add(() async {});
+      q.add(() async {});
     });
   });
 }
