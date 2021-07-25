9ecd790b7358d3b5a1cf21c47191561cae659414
Rustem Kakimov <ruskakimov@gmail.com>
2021-07-25 15:12:28 +0600

    Test 2 tasks running consec

--- test/task_queue_test.dart
+++ test/task_queue_test.dart
@@ -1,3 +1,4 @@
+import 'package:clock/clock.dart';
 import 'package:fake_async/fake_async.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mooltik/common/data/task_queue.dart';
@@ -20,9 +21,25 @@ void main() {
     });
 
     test('runs two tasks consecutively', () {
-      final q = TaskQueue();
-      q.add(() async {});
-      q.add(() async {});
+      fakeAsync((async) {
+        final q = TaskQueue();
+        final s = clock.stopwatch()..start();
+        final timestamps = [];
+
+        q.add(() async {
+          await Future.delayed(Duration(seconds: 5));
+          timestamps.add(s.elapsed);
+        });
+        q.add(() async {
+          await Future.delayed(Duration(seconds: 5));
+          timestamps.add(s.elapsed);
+        });
+
+        async.elapse(Duration(seconds: 10));
+        expect(timestamps, [Duration(seconds: 5), Duration(seconds: 10)]);
+
+        s.stop();
+      });
     });
   });
 }
