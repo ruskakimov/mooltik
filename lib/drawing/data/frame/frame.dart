b4366ec8d11894837c78e791d5b88e60d209e33b
Rustem Kakimov <ruskakimov@gmail.com>
2021-08-18 13:58:14 +0600

    Default to 5fps

--- lib/drawing/data/frame/frame.dart
+++ lib/drawing/data/frame/frame.dart
@@ -10,7 +10,7 @@ import 'package:path/path.dart' as p;
 class Frame extends TimeSpan with EquatableMixin {
   Frame({
     required this.image,
-    Duration duration = const Duration(milliseconds: 80),
+    Duration duration = const Duration(milliseconds: 40 * 5),
   }) : super(duration);
 
   final DiskImage image;
