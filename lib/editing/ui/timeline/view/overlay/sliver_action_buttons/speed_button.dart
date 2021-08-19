31891cb2c5f492ecbdba8ffa9978d81911d2fc99
Rustem Kakimov <ruskakimov@gmail.com>
2021-08-19 11:24:14 +0600

    Slider steppers

--- lib/editing/ui/timeline/view/overlay/sliver_action_buttons/speed_button.dart
+++ lib/editing/ui/timeline/view/overlay/sliver_action_buttons/speed_button.dart
@@ -58,6 +58,12 @@ class SpeedDialog extends StatefulWidget {
 class _SpeedDialogState extends State<SpeedDialog> {
   int animateOn = 5;
 
+  void setAnimateOn(int value) {
+    setState(() {
+      animateOn = value.clamp(1, fps);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,17 +78,33 @@ class _SpeedDialogState extends State<SpeedDialog> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              AnimatedLayerPreview(
-                frames: widget.frames,
-                frameDuration: singleFrameDuration * animateOn,
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: AnimatedLayerPreview(
+                  frames: widget.frames,
+                  frameDuration: singleFrameDuration * animateOn,
+                ),
               ),
-              AppSlider(
-                value: 1 - (animateOn - 1) / (fps - 1),
-                onChanged: (value) {
-                  setState(() {
-                    animateOn = ((1 - value) * (fps - 1) + 1).round();
-                  });
-                },
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.remove),
+                    onPressed: () => setAnimateOn(animateOn + 1),
+                  ),
+                  Expanded(
+                    child: AppSlider(
+                      value: 1 - (animateOn - 1) / (fps - 1),
+                      onChanged: (value) {
+                        setAnimateOn(((1 - value) * (fps - 1) + 1).round());
+                      },
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () => setAnimateOn(animateOn - 1),
+                  ),
+                ],
               ),
             ],
           ),
