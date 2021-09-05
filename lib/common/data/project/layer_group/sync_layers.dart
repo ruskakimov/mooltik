c2ab449bd33f36874c6742244cb3dd32a3dce50c
Rustem Kakimov <ruskakimov@gmail.com>
2021-09-05 14:44:48 +0600

    Refactor tests

--- lib/common/data/project/layer_group/sync_layers.dart
+++ lib/common/data/project/layer_group/sync_layers.dart
@@ -1,5 +1,19 @@
 import 'package:mooltik/common/data/project/scene_layer.dart';
 
-/// Synchronizes frame duration between 2 layers.
-/// Preserves durations of all frames in [primary].
+/// Synchronizes frame count and timing between 2 layers.
+/// Preserves [primary] frame timing and playmode.
+/// Appends empty frames to the shortest layer.
 void syncLayers(SceneLayer primary, SceneLayer secondary) {}
+
+/// Whether 2 layers have identical frame count and timing.
+bool areSynced(SceneLayer a, SceneLayer b) {
+  if (a.playMode != b.playMode) return false;
+  if (a.frameSeq.length != b.frameSeq.length) return false;
+  if (a.frameSeq.totalDuration != b.frameSeq.totalDuration) return false;
+
+  for (int i = 0; i < a.frameSeq.length; i++) {
+    if (a.frameSeq[i].duration != b.frameSeq[i].duration) return false;
+  }
+
+  return true;
+}
--- test/layer_group/sync_layers_test.dart
+++ test/layer_group/sync_layers_test.dart
@@ -1,37 +1,74 @@
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mooltik/common/data/io/disk_image.dart';
+import 'package:mooltik/common/data/project/fps_config.dart';
 import 'package:mooltik/common/data/project/layer_group/sync_layers.dart';
 import 'package:mooltik/common/data/project/scene_layer.dart';
+import 'package:mooltik/common/data/sequence/sequence.dart';
+import 'package:mooltik/drawing/data/frame/frame.dart';
 
-String raw(int testId, String name) {
-  final str =
-      File('./test/layer_group/data/$testId/$name.json').readAsStringSync();
-  return jsonEncode(jsonDecode(str));
+SceneLayer layer(
+  List<int> frameCounts, [
+  PlayMode playMode = PlayMode.extendLast,
+]) {
+  return SceneLayer(
+    Sequence<Frame>(
+      frameCounts
+          .map((frameCount) => Frame(
+                image: DiskImage(file: File('image.png')),
+                duration: singleFrameDuration * frameCount,
+              ))
+          .toList(),
+    ),
+    playMode,
+  );
+}
+
+bool matches(SceneLayer layer, List<int> frameCounts) {
+  if (frameCounts.length != layer.frameSeq.length) return false;
+
+  for (int i = 0; i < frameCounts.length; i++) {
+    final expectedDuration = singleFrameDuration * frameCounts[i];
+    final actualDuration = layer.frameSeq[i].duration;
+
+    if (actualDuration != expectedDuration) return false;
+  }
+
+  return true;
 }
 
 void main() {
-  group('syncLayers', () {
-    test('1: does nothing if layers are synced', () {
-      final a = raw(1, 'a');
-      final b = raw(1, 'b');
-      final aL = SceneLayer.fromJson(jsonDecode(a), '');
-      final bL = SceneLayer.fromJson(jsonDecode(b), '');
-      syncLayers(aL, bL);
-      expect(jsonEncode(aL), a);
-      expect(jsonEncode(bL), b);
+  group('areSynced', () {
+    test('returns true if layers are synced', () {
+      final a = layer([1, 2, 2, 10]);
+      final b = layer([1, 2, 2, 10]);
+      expect(areSynced(a, b), true);
     });
 
-    test('2: appends frames to secondary if primary is longer', () {
-      final a = raw(2, 'a');
-      final b = raw(2, 'b');
-      final aL = SceneLayer.fromJson(jsonDecode(a), '');
-      final bL = SceneLayer.fromJson(jsonDecode(b), '');
-      syncLayers(aL, bL);
-      expect(jsonEncode(aL), a);
-      expect(jsonEncode(bL), raw(2, 'b2'));
-      // TODO: Check if appended frames in bL are empty
+    test('returns false if layers are not synced', () {
+      final a = layer([1, 2, 2, 10]);
+      final b = layer([1, 2, 1, 10]);
+      expect(areSynced(a, b), false);
+    });
+  });
+
+  group('syncLayers', () {
+    test('does nothing if layers are synced', () {
+      final a = layer([1, 2, 3]);
+      final b = layer([1, 2, 3]);
+      expect(areSynced(a, b), true, reason: 'Are initially synced');
+      syncLayers(a, b);
+      expect(areSynced(a, b), true);
+    });
+
+    test('appends frames to secondary if primary is longer', () {
+      final a = layer([1, 2, 3, 4, 5]);
+      final b = layer([1, 2, 3]);
+      syncLayers(a, b);
+      expect(matches(a, [1, 2, 3, 4, 5]), true);
+      expect(matches(b, [1, 2, 3, 4, 5]), true);
+      expect(areSynced(a, b), true);
     });
   });
 }
