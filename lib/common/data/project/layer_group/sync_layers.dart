a71adcff981cdb4f2e117c343405d491a2b89aee
Rustem Kakimov <ruskakimov@gmail.com>
2021-09-05 14:59:04 +0600

    Pass test

--- lib/common/data/project/layer_group/sync_layers.dart
+++ lib/common/data/project/layer_group/sync_layers.dart
@@ -3,7 +3,14 @@ import 'package:mooltik/common/data/project/scene_layer.dart';
 /// Synchronizes frame count and timing between 2 layers.
 /// Preserves [primary] frame timing and playmode.
 /// Appends empty frames to the shortest layer.
-void syncLayers(SceneLayer primary, SceneLayer secondary) {}
+void syncLayers(SceneLayer primary, SceneLayer secondary) {
+  if (primary.frameSeq.length == secondary.frameSeq.length) {
+    for (int i = 0; i < primary.frameSeq.length; i++) {
+      final primaryDuration = primary.frameSeq[i].duration;
+      secondary.frameSeq.changeSpanDurationAt(i, primaryDuration);
+    }
+  }
+}
 
 /// Whether 2 layers have identical frame count and timing.
 bool areSynced(SceneLayer a, SceneLayer b) {
