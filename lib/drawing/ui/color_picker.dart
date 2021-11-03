21e73e4eb0f9947858c386136f6f00e79685f532
Rustem Kakimov <ruskakimov@gmail.com>
2021-11-03 14:39:11 +0600

    Remove unused code

--- lib/drawing/ui/color_picker.dart
+++ lib/drawing/ui/color_picker.dart
@@ -3,8 +3,6 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:mooltik/drawing/ui/color_wheel.dart';
 
-const _buttonWidth = 36.0;
-
 class ColorPicker extends StatelessWidget {
   const ColorPicker({
     Key? key,
@@ -26,60 +24,4 @@ class ColorPicker extends StatelessWidget {
       ],
     );
   }
-}
-
-class _ColorOption extends StatelessWidget {
-  const _ColorOption({
-    Key? key,
-    required this.color,
-    required this.selected,
-    required this.onSelected,
-  }) : super(key: key);
-
-  final Color color;
-  final bool selected;
-  final void Function(Color)? onSelected;
-
-  @override
-  Widget build(BuildContext context) {
-    return Listener(
-      onPointerDown: (_) {
-        onSelected?.call(color);
-      },
-      child: SizedBox(
-        width: _buttonWidth,
-        height: _buttonWidth,
-        child: selected
-            ? _addSelectionRing(ColoredBox(color: color))
-            : ColoredBox(color: color),
-      ),
-    );
-  }
-
-  Widget _addSelectionRing(Widget child) => Stack(
-        fit: StackFit.expand,
-        children: [
-          child,
-          Container(
-            margin: const EdgeInsets.all(2),
-            foregroundDecoration: BoxDecoration(
-              border: Border.all(
-                width: 8,
-                color: Colors.black38,
-              ),
-              shape: BoxShape.circle,
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.all(4),
-            foregroundDecoration: BoxDecoration(
-              border: Border.all(
-                width: 4,
-                color: Colors.white,
-              ),
-              shape: BoxShape.circle,
-            ),
-          ),
-        ],
-      );
 }
